dc6b6ec833467efdf23d9c3f07ad9a67888ca85e
marianphillips <79763160+marianphillips@users.noreply.github.com>
2022-01-12 18:11:10 +0000

    removed a lot of unecessary comments

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -13,31 +13,22 @@ describe("Basket", () => {
 
   
   it("new basket is empty array", () => {
-    // set up
     const expected = []
-    // execute
-    // const result = todoList.create("turn the heating on!")
-    // verify
     expect(basket.items).toEqual(expected)
   })
 
   it("add items to basket", () => {
-    // set up
     const expected = [{
       "sku": "BGLO",
       "price": "0.49",
       "name": "Bagel",
       "variant": "Onion"
     }]
-    // execute
-    // const result = todoList.create("turn the heating on!")
-    // verify
     basket.add("BGLO")
     expect(basket.items).toEqual(expected)
   })
 
   it("add existing items to basket", () => {
-    // set up
     const expected = [{
       "sku": "BGLO",
       "price": "0.49",
@@ -50,67 +41,31 @@ describe("Basket", () => {
       "name": "Bagel",
       "variant": "Onion"
     }]
-    // execute
-    // const result = todoList.create("turn the heating on!")
-    // verify
     basket.add("BGLO")
     expect(basket.add("BGLO")).toEqual("This item is already in your basket")
     expect(basket.items).toEqual(expected)
   })
 
   it("remove items to basket", () => {
-    // set up
     const expected = [{
       "sku": "BGLS",
       "price": "0.49",
       "name": "Bagel",
       "variant": "Sesame"
     }]
-
-    const expectedTwo = {
-      "sku": "BGLO",
-      "price": "0.49",
-      "name": "Bagel",
-      "variant": "Onion"
-    }
-
-    // execute
-    // const result = todoList.create("turn the heating on!")
-    // verify
     basket.add("BGLO")
     basket.add("BGLS")
     basket.remove("BGLO")
-    // expect(basket.remove("BGLO")).toEqual(expectedTwo)
     expect(basket.items).toEqual(expected)
   })
 
   it("know if full and not full", () => {
-    // set up
-    // const expected = [{
-    //   "sku": "BGLS",
-    //   "price": "0.49",
-    //   "name": "Bagel",
-    //   "variant": "Sesame"
-    // }]
-    // execute
-    // const result = todoList.create("turn the heating on!")
-    // verify
     basket.add("BGLO")
     basket.add("BGLS")
     expect(basket.isFull()).toEqual(false)
   })
 
   it("know if full and is full", () => {
-    // set up
-    // const expected = [{
-    //   "sku": "BGLS",
-    //   "price": "0.49",
-    //   "name": "Bagel",
-    //   "variant": "Sesame"
-    // }]
-    // execute
-    // const result = todoList.create("turn the heating on!")
-    // verify
     basket.add("BGLO")
     basket.add("BGLS")
     basket.add("BGLO")
@@ -120,11 +75,7 @@ describe("Basket", () => {
   })
 
   it("know if basket is full so cannot add items", () => {
-    // set up
     const expected = "You cannot add more items, your basket is full"
-    // execute
-    // const result = todoList.create("turn the heating on!")
-    // verify
     basket.add("BGLO")
     basket.add("BGLS")
     basket.add("BGLO")
@@ -135,11 +86,6 @@ describe("Basket", () => {
   })
 
   it("change basket size", () => {
-    // set up
-    // const expected = "You cannot add more items, your basket is full"
-    // execute
-    // const result = todoList.create("turn the heating on!")
-    // verify
     basket.add("BGLO")
     basket.add("BGLS")
     basket.add("BGLO")
@@ -154,23 +100,12 @@ describe("Basket", () => {
   })
 
   it("cannot change basket size to random", () => {
-    // set up
-    // const expected = "You cannot add more items, your basket is full"
-    // execute
-    // const result = todoList.create("turn the heating on!")
-    // verify
-
-    
     expect(basket.changeBasketSize(250)).toEqual(`Pick small, medium or large`)
     expect(basket.size).toEqual(5)
   })
 
   it("change basket size", () => {
-    // set up
     const expected = `Pick a basket bigger than 7`
-    // execute
-    // const result = todoList.create("turn the heating on!")
-    // verify
     basket.add("BGLO")
     basket.add("BGLS")
     basket.add("BGLO")
@@ -186,11 +121,7 @@ describe("Basket", () => {
 
 
   it("cannot remove items that are not in basket", () => {
-    // set up
     const expected = "You cannot remove items that are not in your basket"
-    // execute
-    // const result = todoList.create("turn the heating on!")
-    // verify
     basket.add("BGLO")
     basket.add("BGLS")
     basket.remove("BBB")
@@ -198,22 +129,12 @@ describe("Basket", () => {
   })
 
   it("check if item exists in basket, not there", () => {
-    // set up
-    // const expected = "You cannot remove items that are not in your basket"
-    // execute
-    // const result = todoList.create("turn the heating on!")
-    // verify
     basket.add("BGLO")
     basket.add("BGLS")
     expect(basket.inBasket("BGLW")).toEqual(0)
   })
 
   it("check if item exists in basket, not there", () => {
-    // set up
-    // const expected = "You cannot remove items that are not in your basket"
-    // execute
-    // const result = todoList.create("turn the heating on!")
-    // verify
     basket.add("BGLO")
     basket.add("BGLS")
     expect(basket.inBasket("BGLO")).toEqual(1)
@@ -233,7 +154,6 @@ describe("Basket", () => {
   })
 
   it("check price of item", () => {
-      // const expected = "You cannot remove items that are not in your basket"
     
     expect(basket.priceOfBagel("BGLO")).toEqual("0.49")
   })
