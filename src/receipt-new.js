e2fb4ab875dfff0909b3013463d3efd83a342ebb
marianphillips <79763160+marianphillips@users.noreply.github.com>
2022-01-14 17:17:27 +0000

    updated

--- src/receipt-new.js
+++ src/receipt-new.js
@@ -1,6 +1,7 @@
 const data = require("../inventory-quant.json");
 const INVENTORY = data["inventory"];
 
+
 class Receipt2 {
     constructor(basketCheckout) {
       this.items = basketCheckout;
@@ -9,12 +10,27 @@ class Receipt2 {
 totalEachItem(object, numberOfItemsInDeal, priceOfDeal) {
    
 const numberOfDeals = Math.floor(object.quantity/numberOfItemsInDeal)
-
 const total = numberOfDeals*priceOfDeal + Number(object.price)*(object.quantity - numberOfDeals*numberOfItemsInDeal)
 
 return Math.floor(total * 100) / 100;
 }
 
+printReceiptItems(object, numberOfItems, price) {
+    return `    ${object.variant} ${object.name}`.padEnd(21, " ") + `${object.quantity}`.padStart(2," ") 
+    +`£${this.totalEachItem(object, numberOfItems, price)}`.padStart(8, " ") + `\n`
+    + `(-£${(Math.ceil((Number(object.price)*object.quantity - this.totalEachItem(object, numberOfItems, price))*100)/100).toFixed(2)})\n`.padStart(33, " ")
+    
+}
+
+savingsMessage() {
+    let totalSavings = 0
+    
+    // for loop
+    return `You saved a total of ${totalSavings}
+    on this shop`
+
+ return " "
+}
 
 print(price) {
 
@@ -22,20 +38,18 @@ print(price) {
 
     for(let i = 0; i< this.items.length; i++) {
         if(this.items[i].sku === "BGLO" && this.items[i].quantity >= 6) {
-            receiptItems += `   ${this.items[i].variant} ${this.items[i].name}\t${this.items[i].quantity}\t£${this.totalEachItem(this.items[i], 6, 2.49)}\n`
-            + `\t\t\t(-£${(Math.ceil((Number(this.items[i].price)*this.items[i].quantity - this.totalEachItem(this.items[i], 6, 2.49))*100)/100).toFixed(2)})\n`
-                     
+            receiptItems += this.printReceiptItems(this.items[i], 6, 2.49)       
         }
         else if(this.items[i].sku === "BGLE" && this.items[i].quantity >= 6) {
-            receiptItems += `   ${this.items[i].variant} ${this.items[i].name}\t${this.items[i].quantity}\t£${this.totalEachItem(this.items[i], 6, 2.49)}\n`
-              + `\t\t\t(-£${(Math.ceil((Number(this.items[i].price)*this.items[i].quantity - this.totalEachItem(this.items[i], 6, 2.49))*100)/100).toFixed(2)})\n`  
+            receiptItems += this.printReceiptItems(this.items[i], 6, 2.49)
         }
         else if(this.items[i].sku === "BGLP" && this.items[i].quantity >= 12) {
-            receiptItems += `   ${this.items[i].variant} ${this.items[i].name}\t${this.items[i].quantity}\t£${this.totalEachItem(this.items[i], 12, 3.99)}\n`
-             + `\t\t\t(-£${(Math.ceil((Number(this.items[i].price)*this.items[i].quantity - this.totalEachItem(this.items[i], 12, 3.99))*100)/100).toFixed(2)})\n`  
+            receiptItems += this.printReceiptItems(this.items[i], 12, 3.99)
+
         }
         else {
-        receiptItems += `   ${this.items[i].variant} ${this.items[i].name}\t${this.items[i].quantity}\t£${Number(this.items[i].price)*this.items[i].quantity}\n` 
+        receiptItems +=  `    ${this.items[i].variant} ${this.items[i].name}`.padEnd(21, " ") + `${this.items[i].quantity}`.padStart(2," ") 
+        +`£${Number(this.items[i].price)*this.items[i].quantity}`.padStart(8, " ") + `\n`
         }
     }
     
@@ -47,13 +61,11 @@ print(price) {
        ${Date().substring(3,24)}
     
     ----------------------------
-    
     ${receiptItems}
-    
     ----------------------------
     Total                 £${price.toFixed(2)}
-    
-    
+
+    ${this.savingsMessage()}
            Thank you
          for your order!
     
@@ -64,12 +76,13 @@ print(price) {
 }
 
 const exampleBasket = [
-    { sku: 'BGLO', price: '0.49', name: 'Bagel', variant: 'Onion', quantity: 8 },
-    { sku: 'BGLS', price: '0.49', name: 'Bagel', variant: 'Sesame', quantity: 2 },
-    { sku: 'BGLP', price: '0.39', name: 'Bagel', variant: 'Plain', quantity: 14 },
+    { sku: 'BGLO', price: '0.49', name: 'Bagel', variant: 'Onion', quantity: 6 },
+    { sku: 'BGLE', price: '0.49', name: 'Bagel', variant: 'Everything', quantity: 2 },
+    { sku: 'BGLP', price: '0.39', name: 'Bagel', variant: 'Plain', quantity: 14 }
   ]
 
   let receipt = new Receipt2(exampleBasket)
   console.log(receipt.print(10))
+  console.log(bloop)
 
 module.exports = Receipt2
