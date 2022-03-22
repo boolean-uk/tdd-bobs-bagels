da172ec9b9e18121c64b8b3f96d44c052f3dc94a
Anica Rahman <anicarahman2009@yahoo.it>
2022-03-22 12:37:01 +0000

    JS spec and src foundation

---
+++ spec/support/basket.spec.js
@@ -0,0 +1,10 @@
+const Basket = require("../src/basket.js");
+
+describe(),
+  function () {
+    it("", function () {
+      //set up
+      //execute
+      //verify
+    });
+  };
---
+++ src/basket.js
@@ -0,0 +1,7 @@
+class Basket {
+  constructor() {
+    this.basketList = [];
+  }
+}
+
+module.exports = Basket;
