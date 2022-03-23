cc33191f2b107db453bc57d596e8e003cdd1b3af
Toby Carlo <tobycarlo.c@gmail.com>
2022-03-23 09:52:27 +0000

    done requirements 1, 2, 3

--- src/basket.js
+++ src/basket.js
@@ -1,12 +1,61 @@
 class Bagel {
   constructor() {
     this.basket = [];
+    this.capacity = 5;
+    this.priceList = {
+      plain: 2,
+      cheese: 3,
+      cinnamon: 3,
+      raisin: 3,
+      poppy: 4,
+      sesame: 4,
+    };
   }
 
-  addToBasket(name) {
-    this.basket.push(name);
+  addToBasket(name, num = 1) {
+    const bagelNameArr = Object.keys(this.priceList);
+    if (!bagelNameArr.includes(name)) return "Please add bagels from the list";
 
+    for (let i = 0; i < num; i++) {
+      this.basket.push(name);
+    }
     return this.basket;
+  }
+
+  removeFromBasket(name) {
+    return this.basket.includes(name)
+      ? this.basket.filter((bagel) => bagel !== name)
+      : "You have not order this bagel";
+  }
+
+  isFull() {
+    const bagelsLeft = this.capacity - this.basket.length;
+
+    return this.basket.length >= this.capacity
+      ? "Your basket is full"
+      : `Continue to order; ${bagelsLeft} bagels left`;
+  }
+
+  createBigBasket() {
+    if (this.basket.length >= this.capacity) this.capacity = 12;
+  }
+
+  checkPrice(name) {
+    const priceListArr = Object.entries(this.priceList);
+    const nameAndPrice = priceListArr.find(
+      (priceList) => priceList[0] === name
+    );
+
+    return `bagel: ${nameAndPrice[0]}, price: $${nameAndPrice[1]}`;
+  }
+
+  checkOut() {
+    const bagelPriceArr = this.basket.map((bagel) => this.priceList[bagel]);
+    const totalSum = bagelPriceArr.reduce(
+      (firstPrice, nextPrice) => firstPrice + nextPrice,
+      0
+    );
+    return `total: $${totalSum}`;
   }
 }
 
