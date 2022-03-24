b07a7776df48253d3145585db97fc9d5bc94397e
Anica Rahman <anicarahman2009@yahoo.it>
2022-03-24 17:43:48 +0000

    Added comments

--- src/basket.js
+++ src/basket.js
@@ -22,6 +22,43 @@ class Basket {
     return this.basketList;
   }
 
+  // Nathan's remove solution (part 1-2)
+  /* 
+  remove(id) {
+    const newItems = [];
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i].id != id) {
+        newItems.push(this.items[i])
+      }
+    }
+    this.items = newItems;
+
+    return this.items
+  }
+  */
+
+  // Nathan's solution to part 2 - 3
+  /* 
+  remove(id) {
+    const newItems = [];
+    let foundItem = false;
+
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i].id != id) {
+        newItems.push(this.items[i])
+        continue
+      }
+      fountItem = true
+    }
+    if (!founItem) {
+      return false;
+    }
+    this.items = newItems;
+
+    return this.items
+  }
+  */
+
   fullBasket() {
     if (this.basketList.length >= this.capacity) {
       return true;
