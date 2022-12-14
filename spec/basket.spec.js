9ad247fb330f21421ecf148bfc140f89c9a849c5
Dave <Daverevell491@gmail.com>
2022-12-14 13:06:55 +0000

    Second test failing

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -6,7 +6,7 @@ describe('Basket:', () => {
     basket = new Basket()
   })
 
-  it('a bagel/item can be added to the basket', () => {
+  it('should add a bagel/item to the basket', () => {
     const expected = {
       sku: 'BGLO',
       price: '0.49',
@@ -17,5 +17,65 @@ describe('Basket:', () => {
     const result = basket.addBagel('BGLO')
 
     expect(result).toEqual([expected])
+  })
+
+  it('should return an error when the basket is full and user tries adding more', () => {
+    const expected = [
+      {
+        sku: 'BGLO',
+        price: '0.49',
+        name: 'Bagel',
+        variant: 'Onion'
+      },
+      {
+        sku: 'BGLP',
+        price: '0.39',
+        name: 'Bagel',
+        variant: 'Plain'
+      },
+      {
+        sku: 'BGLE',
+        price: '0.49',
+        name: 'Bagel',
+        variant: 'Everything'
+      },
+      {
+        sku: 'BGLS',
+        price: '0.49',
+        name: 'Bagel',
+        variant: 'Sesame'
+      },
+      {
+        sku: 'COF',
+        price: '0.99',
+        name: 'Bagel',
+        variant: ''
+      },
+      {
+        sku: 'BGSE',
+        price: '2.99',
+        name: 'Bagel Sandwich',
+        variant: 'Everything',
+        fillings: ['Bacon', 'Egg', 'Cheese']
+      },
+      {
+        sku: 'BGSS',
+        price: '4.99',
+        name: 'Bagel Sandwich',
+        variant: 'Sesame',
+        fillings: ['Cream Cheese', 'Smoked Salmon']
+      }
+    ]
+
+    const expectedError = {
+      sku: 'BGLO',
+      price: '0.49',
+      name: 'Bagel',
+      variant: 'Onion'
+    }
+
+    const result = basket.addBagel(expectedError)
+
+    expect(result).toThrowError('Your basket is full')
   })
 })
