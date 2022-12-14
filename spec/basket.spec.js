d4fd1efb3aab53006f9b8f6820e2f3bf6a1a52f1
Joel Watkins <eck087@yahoo.co.uk>
2022-12-14 15:30:48 +0000

    addBagel function and associated tests implemented

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -1,15 +1,70 @@
 const { Basket } = require('../src/basket')
 
 describe('bobs bagels', () => {
-  it('An instance of Basket class has a basketCapacity of 10 by default', () => {
-    const basket = new Basket()
+  const testBasket = new Basket()
 
-    expect(basket.basketCapacity).toBe(10)
+  beforeEach(() => {
+    testBasket.basket = []
+  })
+
+  it('An instance of Basket class has a basketCapacity of 10 by default', () => {
+    expect(testBasket.basketCapacity).toBe(10)
   })
 
   it('The manager can change the basket capacity', () => {
-    const biggerBakset = new Basket(30)
+    const biggerBasket = new Basket(30)
 
-    expect(biggerBakset.basketCapacity).toBe(30)
+    expect(biggerBasket.basketCapacity).toBe(30)
+  })
+
+  it('addBagel function returns an object with new quantity: 1 property/value', () => {
+    const item = testBasket.addBagel('BGLO')
+
+    expect(item).toEqual({
+      sku: 'BGLO',
+      price: '0.49',
+      name: 'Bagel',
+      variant: 'Onion',
+      quantity: 1
+    })
+  })
+
+  it('addBagel pushes items successfully into main basket', () => {
+    testBasket.addBagel('BGLO')
+    testBasket.addBagel('BGSE')
+
+    expect(testBasket.basket).toEqual([
+      {
+        sku: 'BGLO',
+        price: '0.49',
+        name: 'Bagel',
+        variant: 'Onion',
+        quantity: 1
+      },
+      {
+        sku: 'BGSE',
+        price: '2.99',
+        name: 'Bagel Sandwich',
+        variant: 'Everything',
+        fillings: ['Bacon', 'Egg', 'Cheese'],
+        quantity: 1
+      }
+    ])
+  })
+
+  it('quantity should be increased if bagel already exists in basket', () => {
+    testBasket.addBagel('BGLO')
+    testBasket.addBagel('BGLO')
+    testBasket.addBagel('BGLO')
+
+    expect(testBasket.basket).toEqual([
+      {
+        sku: 'BGLO',
+        price: '0.49',
+        name: 'Bagel',
+        variant: 'Onion',
+        quantity: 3
+      }
+    ])
   })
 })
--- src/basket.js
+++ src/basket.js
@@ -5,9 +5,26 @@ class Basket {
     this.basketCapacity = basketCapacity
     this.basket = []
   }
+
+  addBagel(sku) {
+    const found = inventory.find((bagel) => bagel.sku === sku)
+
+    if (!found) return
+
+    const exists = this.basket.find((bagel) => bagel.sku === found.sku)
+
+    if (exists) {
+      ++exists.quantity
+    } else {
+      found.quantity = 1
+      this.basket.push(found)
+    }
+
+    return found
+  }
 }
 
 // const newBasket = new Basket(12)
-// console.log(newBasket)
+// const basket2 = new Basket()
 
 module.exports = { Basket }
