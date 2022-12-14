13bedd30424339151b52d6b12b37551e73fea5cb
Dave <Daverevell491@gmail.com>
2022-12-14 15:41:05 +0000

    Test 2 passing

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -1,6 +1,7 @@
 const Basket = require('../src/basket.js')
+const { inventory } = require('../inventory.json')
 
-describe('Basket:', () => {
+describe('Basket', () => {
   let basket
   beforeEach(() => {
     basket = new Basket()
@@ -20,62 +21,13 @@ describe('Basket:', () => {
   })
 
   it('should return an error when the basket is full and user tries adding more', () => {
-    const expected = [
-      {
-        sku: 'BGLO',
-        price: '0.49',
-        name: 'Bagel',
-        variant: 'Onion'
-      },
-      {
-        sku: 'BGLP',
-        price: '0.39',
-        name: 'Bagel',
-        variant: 'Plain'
-      },
-      {
-        sku: 'BGLE',
-        price: '0.49',
-        name: 'Bagel',
-        variant: 'Everything'
-      },
-      {
-        sku: 'BGLS',
-        price: '0.49',
-        name: 'Bagel',
-        variant: 'Sesame'
-      },
-      {
-        sku: 'COF',
-        price: '0.99',
-        name: 'Bagel',
-        variant: ''
-      },
-      {
-        sku: 'BGSE',
-        price: '2.99',
-        name: 'Bagel Sandwich',
-        variant: 'Everything',
-        fillings: ['Bacon', 'Egg', 'Cheese']
-      },
-      {
-        sku: 'BGSS',
-        price: '4.99',
-        name: 'Bagel Sandwich',
-        variant: 'Sesame',
-        fillings: ['Cream Cheese', 'Smoked Salmon']
-      }
-    ]
+    const basketFull = ['BGLO', 'BGLP', 'BGLE', 'BGLS', 'COF', 'BGSE']
+    basketFull.forEach((bagel) => basket.addBagel(bagel))
 
-    const expectedError = {
-      sku: 'BGLO',
-      price: '0.49',
-      name: 'Bagel',
-      variant: 'Onion'
-    }
+    const result = basket.addBagel('BGSS')
 
-    const result = basket.addBagel(expectedError)
-
-    expect(result).toThrowError('Your basket is full')
+    expect(() => {
+      basket.addBagel(result).toThrowError('Your basket is full')
+    })
   })
 })
--- src/basket.js
+++ src/basket.js
@@ -3,12 +3,16 @@ const { inventory } = require('../inventory.json')
 class Basket {
   constructor() {
     this.basketItems = []
-    this.size = 10
+    this.basketSize = 7
   }
 
   addBagel(sku) {
     const bagel = inventory.filter((item) => item.sku === sku)
     this.basketItems.push(bagel)
+    if (this.basketItems.length > this.basketSize) {
+      throw Error('Your basket is full')
+    }
+
     return bagel
   }
 }
