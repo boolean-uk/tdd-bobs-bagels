b3d10e625edca53aa6df753f6906f22e81035fea
Joel Watkins <eck087@yahoo.co.uk>
2022-12-15 11:54:38 +0000

    applyOffers function underway, bugs to fix

---
+++ specialOffers.js
@@ -0,0 +1,19 @@
+module.exports = {
+  specialOffers: [
+    {
+      sku: 'BGLO',
+      amountNeeded: 6,
+      priceOffer: 2.49
+    },
+    {
+      sku: 'BGLP',
+      amountNeeded: 12,
+      priceOffer: 3.99
+    },
+    {
+      sku: 'BGLE',
+      amountNeeded: 6,
+      priceOffer: 2.49
+    }
+  ]
+}
--- src/basket.js
+++ src/basket.js
@@ -1,4 +1,5 @@
 const { inventory } = require('../inventory.json')
+const { specialOffers } = require('../specialOffers')
 
 class Basket {
   constructor(basketCapacity = 10) {
@@ -13,7 +14,6 @@ class Basket {
 
     const exists = this.basket.find((bagel) => bagel.sku === found.sku)
 
-    // fix this line
     if (exists) ++exists.quantity
     else {
       if (this.basket.length === this.basketCapacity) return false
@@ -41,16 +41,66 @@ class Basket {
   }
 
   displayTotal() {
+    // const offerData = this.applyOffers()
+    // console.log(offerData)
+
     const total = this.basket.reduce((total, bagel) => {
       return total + +bagel.price * bagel.quantity
     }, 0)
 
     return `${total.toFixed(2)}`
   }
+
+  applyOffers() {
+    let offersTotal = 0
+
+    specialOffers.forEach((offer) => {
+      this.basket.forEach((bagel) => {
+        if (bagel.sku === offer.sku && bagel.quantity >= offer.amountNeeded) {
+          console.log('there are eligible offers')
+          const remainingAfterOffer = bagel.quantity % offer.amountNeeded
+          const offerAmount = bagel.quantity - remainingAfterOffer
+
+          // work out offer totals
+          offersTotal += offer.priceOffer * (offerAmount / offer.amountNeeded)
+        }
+      })
+    })
+    return offersTotal.toFixed(2)
+  }
 }
 
 const newBasket = new Basket(12)
 newBasket.addBagel('BGLO')
-console.log(newBasket.basket)
+newBasket.addBagel('BGLO')
+newBasket.addBagel('BGLO')
+newBasket.addBagel('BGLO')
+newBasket.addBagel('BGLO')
+newBasket.addBagel('BGLO')
+newBasket.addBagel('BGLO')
+newBasket.addBagel('BGLO')
+newBasket.addBagel('BGLO')
+newBasket.addBagel('BGLO')
+newBasket.addBagel('BGLO')
+newBasket.addBagel('BGLO')
+newBasket.addBagel('BGLO')
+newBasket.addBagel('BGLO')
+
+newBasket.addBagel('BGLP')
+newBasket.addBagel('BGLP')
+newBasket.addBagel('BGLP')
+newBasket.addBagel('BGLP')
+newBasket.addBagel('BGLP')
+newBasket.addBagel('BGLP')
+newBasket.addBagel('BGLP')
+newBasket.addBagel('BGLP')
+newBasket.addBagel('BGLP')
+newBasket.addBagel('BGLP')
+newBasket.addBagel('BGLP')
+newBasket.addBagel('BGLP')
+
+// console.log(newBasket.displayTotal())
+const data = newBasket.applyOffers()
+console.log('returning from applyOffers:', data)
 
 module.exports = { Basket }
