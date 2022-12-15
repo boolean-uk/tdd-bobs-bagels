dfdb9ba6a18ab68a1d647eab3f524366d4b144d2
wagnerAJS221 <wagneralex221@gmai.com>
2022-12-15 13:54:30 +0000

    remove test passing now

--- src/basket.js
+++ src/basket.js
@@ -7,7 +7,27 @@ function add(bagel) {
   return basket
 }
 
-function remove(sku) {}
+function remove(sku) {
+  const skuToRemove = 'BGLO'
+  const filterBagels = basket.filter((bagel) => bagel.sku !== skuToRemove)
+
+  return filterBagels
+}
+// const people = [
+//   { id: 1, name: 'serdar' },
+//   { id: 5, name: 'alex' },
+//   { id: 300, name: 'brittany' }
+// ]
+
+// const idToRemove = 5
+
+// const filteredPeople = people.filter((item) => item.id !== idToRemove)
+
+// console.log(filteredPeople)
+// [
+//   { id: 1, name: 'serdar' },
+//   { id: 300, name: 'brittany' }
+// [
 
 function basketReset() {
   basket.splice(0, basket.length)
@@ -18,18 +38,3 @@ module.exports = {
   basketReset,
   remove
 }
-
-// const people = [
-//   { id: 1, name: 'serdar' },
-//   { id: 5, name: 'alex' },
-//   { id: 300, name: 'brittany' }
-// ];
-
-// const idToRemove = 5;
-
-// const filteredPeople = people.filter((item) => item.id !== idToRemove);
-
-// // [
-// //   { id: 1, name: 'serdar' },
-// //   { id: 300, name: 'brittany' }
-// // [
