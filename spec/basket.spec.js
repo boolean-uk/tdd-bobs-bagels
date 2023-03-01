670311822e83f10272bc21da67bd4183a10c6a91
Kat De Mey <katdemey@outlook.com>
2023-03-01 16:12:48 +0000

    Part 1 complete

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -1,16 +1,13 @@
-const {Basket} = require('./../src/basket')
+const { Basket } = require('./../src/basket')
 
 describe("Basket contents:", () => {
     it("(1) should add a bagel to the basket", () => {
         //setup
-        //we need the sku value from inventory.js ; manually written bec .js not imported
+
         const sku = 'BGSE';
-        //creating a new basket for testing purposes: 
-        //reset things to fresh on each test so that the order of testing is not nb. think of the SoftPlay tdd reseting occupancy to (0, 0)
         const basket = new Basket()
 
         //execute:
-        //when function is called, I want to see the Basket/basket = [{newBagel}]
         const expectedResult = {
             sku: 'BGSE',
             price: '2.99',
@@ -19,7 +16,7 @@ describe("Basket contents:", () => {
             fillings: ['Bacon', 'Egg', 'Cheese']
         };
 
-        //linking it to basket. becuase  was getting "function is undefined" errors. result of the function being called
+
         const result = basket.addToBasket(sku);
         //verify
         expect(result).toEqual(expectedResult)
@@ -27,19 +24,36 @@ describe("Basket contents:", () => {
 
     it("(2) should remove a bagel from the basket", () => {
         //setup
-        const sku = 'BGSE';
-        const basket = new Basket()
+        const sku = 'BGLO';
+        const newBasket = new Basket()
+
+        newBasket.basket.push(
+            {
+                sku: 'BGLO',
+                price: '0.49',
+                name: 'Bagel',
+                variant: 'Onion'
+            },
+            {
+                sku: 'BGLP',
+                price: '0.39',
+                name: 'Bagel',
+                variant: 'Plain'
+            })
 
         //execute:
-        const expectedResult = {
-            sku: 'BGSE',
-            price: '2.99',
-            name: 'Bagel Sandwich',
-            variant: 'Everything',
-            fillings: ['Bacon', 'Egg', 'Cheese']
-        };
+        const expectedBasket = new Basket()
+        expectedBasket.basket.push({
+            sku: 'BGLP',
+            price: '0.39',
+            name: 'Bagel',
+            variant: 'Plain'
+        })
+        const expectedResult = expectedBasket
 
-        const result = basket.removeFromBasket(sku);
+        newBasket.removeFromBasket(sku);
+
+        const result = newBasket
         //verify
         expect(result).toEqual(expectedResult)
     });
--- src/basket.js
+++ src/basket.js
@@ -16,13 +16,31 @@ class Basket {
         } else {
             return "this bagel does not exist"
         }
-    
+    }
+
+    removeFromBasket(sku) {
+        // const removedBagel = this.basket.find(bagel => bagel.sku === sku)
+        const removedBagel = (bagel) => bagel.sku === sku
+        
+        const index = this.basket.findIndex(removedBagel)
+
+        if (removedBagel) {
+            const basket = new Basket()
+            this.basket.splice(index, 1)
+        } else {
+            return "this bagel is not in your basket"
+        }
     }
 }
 
+
 // const basket = new Basket()
-// basket.addToBasket('BGSE')
-// console.log(Basket.basket)
+// basket.addToBasket('BGLO')
+// console.log(basket)
+
+// // const basket = new Basket()
+// basket.removeFromBasket('BGLO')
+// console.log(basket)
 
 module.exports = {
     Basket
