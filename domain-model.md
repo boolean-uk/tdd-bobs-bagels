219159871d15b85a386963a9ae1c1ede1237c401
Akis-Hajittofi <akis.hajittofi@hotmail.co.uk>
2023-03-03 11:49:18 +0000

    Part 2 done

--- domain-model.md
+++ domain-model.md
@@ -1,7 +1,5 @@
 # Domain Model
 
-## Part 1 Model
-
 js object Bagel
 PROPERTIES
 
@@ -14,12 +12,27 @@ class Basket
 PROPERTIES
 
 - basket: Array[Bagel js objects]
+- capacity: Number
 
-METHODS
-addToBasket(bagelItem: Bagel js object) -> Adds an item to the basket
-INPUT: Bagel JS Object
-OUTPUT: True/false
+METHODS  
+addToBasket(bagelItem: Bagel js object) -> Adds an item to the basket. If the the basket capacity has been reached, then the item won't be added.  
+INPUT: Bagel JS Object  
+OUTPUTS:
 
-removeFromBasket(sku: string) -> Removes an item from the basket
-INPUT: sku string for a bagel object
-OUTPUT: True/false
+- True -> Item added successfully
+- False -> Item not added
+- "full" -> If the basket capacity has been reached
+
+removeFromBasket(sku: string) -> Removes an item from the basket. If the item to be removed doesn't exist in the basket then the user must know.
+INPUTS:
+
+- sku string for a bagel object
+- capacity number
+
+OUTPUTS:
+True -> Item removed successfully
+False -> Item doesn't exist in basket
+
+increaseCapacity(newCapacity: number) -> Increases the capacity by given number
+INPUT: capacity: number
+OUTPUT: capacity
--- src/basket.js
+++ src/basket.js
@@ -1,31 +1,46 @@
 require('./../inventory.json')
 
 class Basket {
-  constructor(basket = []) {
+  constructor(basket = [], capacity = 3) {
     this.basket = basket
+    this.capacity = capacity
   }
 
   addToBasket(bagel) {
-    this.basket.push(bagel)
+    // console.log('LENGTH: ', this.basket.length)
+    if (this.basket.length === this.capacity) {
+      return 'full'
+    } else {
+      this.basket.push(bagel)
+    }
+
     if (this.basket.includes(bagel)) {
       return true
-    } else {
+    } else if (!this.basket.includes(bagel)) {
       return false
     }
   }
 
   removeFromBasket(sku) {
+    let isTrue = null
     const filteredList = this.basket.filter(
       (bagelItem) => bagelItem.sku !== sku
     )
-    this.basket = filteredList
-    let isTrue = true
-    this.basket.forEach((bagelItem) => {
-      if (bagelItem.sku === sku) {
-        isTrue = false
-      }
-    })
+    const findItem = this.basket.findIndex((bagel) => bagel.sku === sku)
+    if (findItem === -1) {
+      // Item doesn't exist
+      isTrue = false
+    } else {
+      // Item exists
+      this.basket = filteredList
+      isTrue = true
+    }
     return isTrue
+  }
+
+  increaseCapacity(newCapacity) {
+    this.capacity += newCapacity
+    return this.capacity
   }
 }
 
