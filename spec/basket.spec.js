7f90641f12fd2646b3506192b0001b30c7a9717e
carolina arruda <carolinacalouroarruda@gmail.com>
2023-06-14 17:25:23 +0200

    working on part 2 bigbasket

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -1,4 +1,5 @@
 const Basket = require('../src/basket')
+const BigBasket = require('../src/basket')
 
 describe('Create the basket', () => {
   let testBasket
@@ -15,8 +16,8 @@ describe('Create the basket', () => {
   })
 
   it('remove bagel from basket', ()=> {
-    testBasket.addBagel('Sprinkle')
-    expect(testBasket.removeBagel('Sprinkle')).toEqual([])
+    testBasket.addBagel('BGLP')
+    expect(testBasket.removeBagel('BGLP')).toEqual([])
   })
   it('remove bagel from basket with more than 1 item', ()=> {
     testBasket.addBagel('BGLO')
@@ -27,5 +28,64 @@ describe('Create the basket', () => {
     testBasket.addBagel('BGLO')
     testBasket.addBagel('BGLP')
     expect(testBasket.removeBagel('Ham')).toEqual([{ sku: "BGLO", price: "0.49", name: "Bagel", variant: "Onion" }, { sku: "BGLP", price: "0.39", name: "Bagel", variant: "Plain" }])
+  })
+  it('try to add a bagel when small basket is full', () => {
+    testBasket.addBagel('BGLO')
+    testBasket.addBagel('BGLP')
+    testBasket.addBagel('BGSS')
+    testBasket.addBagel('BGSE')
+    const basket = [...testBasket.basket]
+    expect(testBasket.addBagel('COF')).toEqual(basket)
+  })
+  it('try to add a bagel when small basket is not full', () => {
+    testBasket.addBagel('BGLO')
+    testBasket.addBagel('BGLP')
+    testBasket.addBagel('BGSS')
+    const basketTwo = [...testBasket.basket]
+    expect(testBasket.addBagel('COF')).toEqual([...basketTwo, {    sku: "COF",
+    price: "0.99",
+    name: "Bagel",
+    variant: ""}])
+  })
+})
+
+describe("Create big basket", () => {
+  let testBasketTwo
+  beforeEach(() => {
+    testBasketTwo = new BigBasket()
+  })
+
+  it('add the new bagel', () => {
+    expect(testBasketTwo.addBagel('BGLO')).toEqual([{ sku: "BGLO", price: "0.49", name: "Bagel", variant: "Onion" }])
+  })
+
+  it('bagel type has not been added', () => {
+    expect(testBasketTwo.addBagel('')).toEqual([])
+  })
+
+  it('remove bagel from basket', ()=> {
+    testBasketTwo.addBagel('BGLP')
+    expect(testBasketTwo.removeBagel('BGLP')).toEqual([])
+  })
+  it('remove bagel from basket with more than 1 item', ()=> {
+    testBasketTwo.addBagel('BGLO')
+    testBasketTwo.addBagel('BGLP')
+    expect(testBasketTwo.removeBagel('BGLP')).toEqual([{ sku: "BGLO", price: "0.49", name: "Bagel", variant: "Onion" }])
+  })
+  it('remove bagel type is not in the cart', () => {
+    testBasketTwo.addBagel('BGLO')
+    testBasketTwo.addBagel('BGLP')
+    expect(testBasketTwo.removeBagel('Ham')).toEqual([{ sku: "BGLO", price: "0.49", name: "Bagel", variant: "Onion" }, { sku: "BGLP", price: "0.39", name: "Bagel", variant: "Plain" }])
+  })
+  it('try to add a bagel when we have 4 bagels in cart', () => {
+    testBasketTwo.addBagel('BGLO')
+    testBasketTwo.addBagel('BGLP')
+    testBasketTwo.addBagel('BGSS')
+    testBasketTwo.addBagel('BGSS')
+    const basketThree = [...testBasketTwo.bigBasket]
+    expect(testBasketTwo.addBagel('COF')).toEqual([...basketThree, {    sku: "COF",
+    price: "0.99",
+    name: "Bagel",
+    variant: ""}])
   })
 })
--- src/basket.js
+++ src/basket.js
@@ -7,8 +7,11 @@ class Basket {
 
   addBagel(SKU) {
     const bagelItem = data.find((item) => item.sku === SKU)
-    if (bagelItem) {
+    if (bagelItem && this.basket.length < 4) {
       this.basket.push(bagelItem)
+    }
+    else{
+      console.log("Your basket is full, please use a bigger basket!");
     }
     return this.basket
   }
@@ -18,11 +21,35 @@ class Basket {
   }
 }
 
-let newTestBasket = new Basket()
+class BigBasket {
+  constructor() {
+    this.bigBasket = []
+  }
+  addBagel(SKU) {
+    const bagelItem = data.find((item) => item.sku === SKU)
+    if (bagelItem) {
+      this.bigBasket.push(bagelItem)
+    }
 
-// console.log(newTestBasket);
-// console.log(newTestBasket.addBagel('cheese'));
-// console.log(newTestBasket.addBagel('ham'));
-// console.log(newTestBasket.removeBagel('ham'));
+    return this.bigBasket
+  }
 
-module.exports = Basket
+  removeBagel(sku) {
+    return this.bigBasket = this.bigBasket.filter((item)=> item.sku !== sku)
+  }
+}
+
+
+
+let newTestBasket = new BigBasket()
+
+console.log(newTestBasket);
+console.log(newTestBasket.addBagel('COF'));
+console.log(newTestBasket.addBagel('COF'));
+console.log(newTestBasket.addBagel('COF'));
+console.log(newTestBasket.addBagel('COF'));
+console.log(newTestBasket.addBagel('COF'));
+
+
+
+module.exports = Basket, BigBasket
