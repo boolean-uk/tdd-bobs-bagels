8ca3af2c7af71dea20552d8d920be199a95398d1
Lee Kelly <leeowenkelly@gmail.com>
2023-06-14 15:11:24 +0100

    add and remove with fetch

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -1,20 +1,32 @@
-import Basket from "../src/basket.js"
+import Basket from '../src/basket.js'
 
-describe("Basket", () => {
-    // to be more descriptive, add another describe (useful when working with more than one class)
-    describe("addItem", () => {
-        it("add item to basket", async () => {
-        //    SET UP 
-        const newBasket = new Basket()
-        const expected = [{ sku: 'BGLE', price: '0.49', name: 'Bagel', variant: 'Everything' }]
-        
-// EXECUTE
+describe('Basket', () => {
+  // to be more descriptive, add another describe (useful when working with more than one class)
+  describe('addItem', () => {
+    it('add item to basket', async () => {
+      // SET UP
+      const newBasket = new Basket()
+      const expected = [
+        { sku: 'BGLE', price: '0.49', name: 'Bagel', variant: 'Everything' }
+      ]
 
-const result = await newBasket.addItem("BGLE")
-// VERIFY
-expect(result).toEqual(expected)
-        })
+      // EXECUTE
+
+      const result = await newBasket.addItem('BGLE')
+      // VERIFY
+      expect(result).toEqual(expected)
     })
-    
+  })
+  describe('removeItem', () => {
+    fit('remove item from basket', async () => {
+      // SET UP
+      const newBasket = new Basket()
+      await newBasket.addItem('BGLE')
+      const expected = []
+      // EXECUTE
+      const result = await newBasket.removeItem('BGLE')
+      // VERIFY
+      expect(result).toEqual(expected)
+    })
+  })
 })
-
--- src/basket.js
+++ src/basket.js
@@ -1,36 +1,44 @@
+// import inventory from '../inventory.json'
+
 class Basket {
-    constructor() {
-      this.basket = [];
-    }
-  
-    async addItem(sku) {
-      try {
-        const response = await fetch("http://localhost:4000/inventory");
-        const data = await response.json();
-  
-        const bagel = data.find(item => item.sku === sku);
-        if (bagel) {
+  constructor() {
+    this.basket = []
+  }
+
+  async addItem(sku) {
+    try {
+      const response = await fetch('http://localhost:4000/inventory')
+      const data = await response.json()
+
+      const bagel = data.find((item) => item.sku === sku)
+      if (bagel) {
         //   console.log(bagel);
-          this.basket.push(bagel);
-          console.log(this.basket)
-          return this.basket;
-        }
-        else{
-            console.log("item not found, try again")
-        } 
-  
-        // console.log("Bagels data:", data);
-      } catch (error) {
-        console.error("Error fetching inventory:", error);
+        this.basket.push(bagel)
+        console.log('basket add', this.basket)
+        return this.basket
+      } else {
+        console.log('item not found, try again')
       }
+
+      // console.log("Bagels data:", data);
+    } catch (error) {
+      console.error('Error fetching inventory:', error)
     }
   }
-  
-  const newBasket = new Basket();
-  newBasket.addItem("BGLE");
 
+  async removeItem(sku) {
+    const bagelIndex = this.basket.findIndex((item) => item.sku === sku)
 
+    if (bagelIndex !== -1) {
+      console.log('item found at index:', bagelIndex)
+      this.basket.splice(bagelIndex, 1)
+      console.log('basket after remove', this.basket)
+    } else {
+      console.log('item not in basket')
+    }
 
-  
-  export default Basket
-  
+    return this.basket
+  }
+}
+
+export default Basket
