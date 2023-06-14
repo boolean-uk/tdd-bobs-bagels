4370a734537168954f1d0372b2ae0fe0a3b6a18a
Alex Shaw <alexjshaw@gmail.com>
2023-06-14 18:03:46 +0100

    addMultipleItems working, checks if number added will overflow basket and returns max number that will fit if so, addItems checks and updates quantity

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -12,18 +12,38 @@ describe('Testing Basket constructor', () => {
 })
 
 describe('Testing addItem', () => {
-  it('Item added successfully', () => {
+  it('Item added to an empty basket', () => {
     // Setup
     const basket = new Basket()
     const testItem = {
       sku: 'BGLO',
       price: '0.49',
       name: 'Bagel',
-      variant: 'Onion'
+      variant: 'Onion',
+      quantity: 1
     }
     // Execution
     const expectedResponse = new Basket()
     expectedResponse.basket.push(testItem)
+    basket.addItem('BGLO')
+    // Check
+    expect(basket).toEqual(expectedResponse)
+  })
+
+  it('Item already exists in basket', () => {
+    // Setup
+    const basket = new Basket()
+    const testItem = {
+      sku: 'BGLO',
+      price: '0.49',
+      name: 'Bagel',
+      variant: 'Onion',
+      quantity: 2
+    }
+    // Execution
+    const expectedResponse = new Basket()
+    expectedResponse.basket.push(testItem)
+    basket.addItem('BGLO')
     basket.addItem('BGLO')
     // Check
     expect(basket).toEqual(expectedResponse)
@@ -49,11 +69,11 @@ describe('Testing addItem', () => {
 
   it('Creating a basket with a custom size', () => {
     // Setup
-    const basket = new Basket(8)
+    const basket = new Basket(15)
     // Execution
     const response = basket.size
     // Check
-    expect(response).toBe(8)
+    expect(response).toBe(15)
   })
 
   it('Gives an error if size is < 0, non-integer or NaN. Creates basket with default size.', () => {
@@ -66,9 +86,9 @@ describe('Testing addItem', () => {
     const responseNegative = basketNegative.size
     const responseNonNumber = basketNonNumber.size
     // Check
-    expect(responseNonInteger).toBe(5)
-    expect(responseNegative).toBe(5)
-    expect(responseNonNumber).toBe(5)
+    expect(responseNonInteger).toBe(10)
+    expect(responseNegative).toBe(10)
+    expect(responseNonNumber).toBe(10)
   })
 
   it('Basket is full', () => {
@@ -86,15 +106,11 @@ describe('Testing removeItem', () => {
   it('Item removed successfully', () => {
     // Setup
     const basket = new Basket()
-    const testItem = {
-      sku: 'BGLO',
-      price: '0.49',
-      name: 'Bagel',
-      variant: 'Onion'
-    }
-    basket.basket.push(testItem)
+    basket.addItem('BGLO')
+    basket.addItem('BGLP')
     // Execution
     const expectedResponse = new Basket()
+    expectedResponse.addItem('BGLP')
     basket.removeItem('BGLO')
     // Check
     expect(basket).toEqual(expectedResponse)
@@ -103,13 +119,7 @@ describe('Testing removeItem', () => {
   it('Entered SKU not found', () => {
     // Setup
     const basket = new Basket()
-    const testItem = {
-      sku: 'BGLO',
-      price: '0.49',
-      name: 'Bagel',
-      variant: 'Onion'
-    }
-    basket.basket.push(testItem)
+    basket.addItem('BGLO')
     // Execution
     const response = basket.removeItem('WRONG')
     // Check
@@ -123,6 +133,114 @@ describe('Testing removeItem', () => {
     const response = basket.removeItem()
     // Check
     expect(response).toBe('No SKU entered')
+  })
+})
+
+describe('Testing checkPrice', () => {
+  it('SKU matches an inventory item', () => {
+    // Setup
+    const basket = new Basket()
+    // Execution
+    const expectedResponse = '0.99'
+    const response = basket.checkPrice('COF')
+    // Check
+    expect(response).toBe(expectedResponse)
+  })
+
+  it('SKU is not correct or no SKU is given', () => {
+    // Setup
+    const basket = new Basket()
+    // Execution
+    const response = basket.checkPrice()
+    // Check
+    expect(response).toBe('Incorrect SKU. Item not found')
+  })
+})
+
+describe('Testing addMultipleItems', () => {
+  it('Item not already in basket', () => {
+    // Setup
+    const basket = new Basket()
+    const testItem = {
+      sku: 'BGLO',
+      price: '0.49',
+      name: 'Bagel',
+      variant: 'Onion',
+      quantity: 3
+    }
+    // Execution
+    const expectedResponse = new Basket()
+    expectedResponse.basket.push(testItem)
+    const response = basket.addMultipleItems('BGLO', 3)
+    // Check
+    expect(response).toEqual(expectedResponse.basket)
+  })
+
+  it('Item already in basket', () => {
+    // Setup
+    const basket = new Basket()
+    basket.addItem('BGLO')
+    const testItem = {
+      sku: 'BGLO',
+      price: '0.49',
+      name: 'Bagel',
+      variant: 'Onion',
+      quantity: 4
+    }
+    // Execution
+    const expectedResponse = new Basket()
+    expectedResponse.basket.push(testItem)
+    const response = basket.addMultipleItems('BGLO', 3)
+    // Check
+    expect(response).toEqual(expectedResponse.basket)
+  })
+
+  it('SKU is not correct or no SKU is given or quantity not given', () => {
+    // Setup
+    const basket = new Basket()
+    // Execution
+    const response = basket.addMultipleItems()
+    // Check
+    expect(response).toBe('Incorrect SKU. Item not found')
+  })
+
+  it('If item will take basket above size, tell user what max they can add is', () => {
+    // Setup
+    const basketTwo = new Basket(2)
+    const basketThree = new Basket(3)
+    // Execution
+    const responseTwo = basketTwo.addMultipleItems('BGLO', 5)
+    const responseThree = basketThree.addMultipleItems('BGLO', 5)
+    // Check
+    expect(responseThree).toEqual('Basket size exceeded. Maximum of 3 allowed')
+    expect(responseTwo).toEqual('Basket size exceeded. Maximum of 2 allowed')
+  })
+})
+
+describe('Testing totalItems', () => {
+  it('Checking count of items is correct', () => {
+    // Setup
+    const basket = new Basket()
+    const testItem1 = {
+      sku: 'BGLO',
+      price: '0.49',
+      name: 'Bagel',
+      variant: 'Onion',
+      quantity: 4
+    }
+    const testItem2 = {
+      sku: 'BGLP',
+      price: '0.39',
+      name: 'Bagel',
+      variant: 'Plain',
+      quantity: 3
+    }
+    // Execution
+    basket.basket.push(testItem1)
+    basket.basket.push(testItem2)
+    const response = basket.totalItems()
+    // Check
+    expect(response).toBe(7)
   })
 })
 
--- src/basket.js
+++ src/basket.js
@@ -1,12 +1,12 @@
 const inventory = require('../inventory.json').inventory
 
 class Basket {
-  constructor(size = 5) {
+  constructor(size = 10) {
     if (!Number.isInteger(size) || size < 1 || typeof size !== 'number') {
       console.error(
         'Invalid size entered, basket size has been set to default (5)'
       )
-      size = 5
+      size = 10
     }
     this.basket = []
     this.size = size
@@ -14,19 +14,28 @@ class Basket {
 
   addItem(sku) {
     if (sku === undefined) {
-      console.error('No SKU Entered')
       return 'No SKU Entered'
-    } else if (this.basket.length === this.size) {
+    } else if (this.totalItems() >= this.size) {
       return 'Basket is full. Item was not added.'
     } else {
       const addedItem = inventory.find((item) => item.sku === sku)
-      if (addedItem) {
-        this.basket.push(addedItem)
+      const alreadyInBasket = this.basket.find((item) => item.sku === sku)
+      if (addedItem && !alreadyInBasket) {
+        this.basket.push({ ...addedItem, quantity: 1 })
+        return this.basket
+      } else if (addedItem && alreadyInBasket) {
+        alreadyInBasket.quantity++
         return this.basket
       } else {
         return 'Chosen item not found'
       }
     }
+  }
+
+  totalItems() {
+    let itemCount = 0
+    this.basket.forEach((item) => (itemCount += item.quantity))
+    return itemCount
   }
 
   removeItem(sku) {
@@ -43,7 +52,65 @@ class Basket {
       }
     }
   }
+
+  checkPrice(sku) {
+    if (sku === undefined) {
+      return 'Incorrect SKU. Item not found'
+    } else {
+      const itemToCheck = inventory.find((item) => item.sku === sku)
+      if (itemToCheck) {
+        return itemToCheck.price
+      } else {
+        return 'Incorrect SKU. Item not found'
+      }
+    }
+  }
+
+  addMultipleItems(sku, quantity) {
+    if (sku === undefined || isNaN(quantity)) {
+      return 'Incorrect SKU. Item not found'
+    } else if (this.totalItems() >= this.size) {
+      return 'Basket is full. Item was not added.'
+    } else if (this.totalItems() + quantity > this.size) {
+      const maxAllowed = this.size - this.totalItems()
+      return `Basket size exceeded. Maximum of ${maxAllowed} allowed`
+    } else {
+      const addedItem = inventory.find((item) => item.sku === sku)
+      const alreadyInBasket = this.basket.find((item) => item.sku === sku)
+      if (addedItem && !alreadyInBasket) {
+        this.basket.push({ ...addedItem, quantity: quantity })
+        return this.basket
+      } else if (addedItem && alreadyInBasket) {
+        alreadyInBasket.quantity += quantity
+        return this.basket
+      } else {
+        return 'Chosen item not found'
+      }
+    }
+  }
 }
+
+/*
+  addItem(sku) {
+    if (sku === undefined) {
+      return 'No SKU Entered'
+    } else if (this.totalItems() >= this.size) {
+      return 'Basket is full. Item was not added.'
+    } else {
+      const addedItem = inventory.find((item) => item.sku === sku)
+      const alreadyInBasket = this.basket.find((item) => item.sku === sku)
+      if (addedItem && !alreadyInBasket) {
+        this.basket.push({ ...addedItem, quantity: 1 })
+        return this.basket
+      } else if (addedItem && alreadyInBasket) {
+        alreadyInBasket.quantity++
+        return this.basket
+      } else {
+        return 'Chosen item not found'
+      }
+    }
+  }
+*/
 
 module.exports = Basket
 
