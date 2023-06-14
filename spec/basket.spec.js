62522b8d888bf9ef606ce7eb257d0c6b59818ab7
marc-alzapiedi <marcalzapiedi@gmail.com>
2023-06-14 13:11:42 +0200

    wrote the delete method

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -10,13 +10,34 @@ describe('basket', () => {
       const newBasket = new Basket()
 
       // Then
-      expect(newBasket.createBagel(bagel)).toEqual([
-        {
-          id: 1,
-          price: 0.49,
-          name: bagel
-        }
-      ])
+      expect(newBasket.createBagel(bagel)).toEqual({
+        id: 1,
+        price: 0.49,
+        name: bagel
+      })
+    })
+
+    describe('getBasket', () => {
+      it('Get the basket (array) with items (objects)', () => {
+        // When
+        const newBasket = new Basket()
+        newBasket.createBagel('Everything Bagel')
+        newBasket.createBagel('Plain Bagel')
+
+        // Then
+        expect(newBasket.getBasket()).toEqual([
+          {
+            id: 1,
+            price: 0.49,
+            name: 'Everything Bagel'
+          },
+          {
+            id: 2,
+            price: 0.49,
+            name: 'Plain Bagel'
+          }
+        ])
+      })
     })
 
     describe('deleteBagel', () => {
--- src/basket.js
+++ src/basket.js
@@ -11,10 +11,23 @@ class Basket {
     }
 
     this.bagelBasket.push(obj)
+    return obj
+  }
+
+  getBasket() {
     return this.bagelBasket
   }
 
-  
+  deleteBagel(bagel) {
+    const newArr = this.bagelBasket.filter((object) => {
+      return object.bagel === bagel
+    })
+    console.log(newArr)
+
+    this.bagelBasket.splice(this.bagelBasket.indexOf(newArr[0]), 1)
+
+    return this.bagelBasket
+  }
 }
 
 module.exports = Basket
