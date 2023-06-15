37acadd84d3ab326f0116011de13e90034829025
gid-ctrl <gideonusenbor@yahoo.co.uk>
2023-06-15 10:30:44 +0100

    remove basket spec test working

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -22,7 +22,7 @@ describe("add to basket", () => {
     expect(basket.items.length).toEqual(1)
 
   })
-
+  
   it('adds a different given item to the basket if not already in the basket', () => {
     //GIVEN
     const item = {
@@ -32,44 +32,46 @@ describe("add to basket", () => {
       "variant": "Plain",
       "quantity": 1
     }
-
+    
     //WHEN
     const res = basket.addToBasket('BGLP')
-
+    
     //THEN
     expect(res).toEqual(item)
     expect(basket.items.length).toEqual(1)
-
   })
-
+})
+  describe('remove from basket', () => {
+    let basket
+  beforeEach(() => { basket = new Basket })
   it('does not add to basket if the given sku does not match an inventory item', () => {
     //GIVEN
-
+    
     //WHEN
     const res = basket.addToBasket('HWLD')
-
+    
     //THEN
     expect(res).toEqual(undefined)
     expect(basket.items.length).toEqual(0)
-
+    
   })
-
-it('should be able to remove items from basket', () => {
-  //GIVEN
-  const basket = new Basket()
-  const item = {
-    "sku": "BGLO",
-    "price": "0.49",
-    "name": "Bagel",
-    "variant": "Onion",
-    "quantity": 1
-  }
-//WHEN
-basket.addToBasket(item) 
-const newRes = basket.removeFromBasket('BGLO')
-const expectRes = undefined
-//THEN
-expect(newRes).toEqual(expectRes)
-})
-
+  
+  it('should be able to remove items from basket', () => {
+    //GIVEN
+    const basket = new Basket()
+    const item = {
+      "sku": "BGLO",
+      "price": "0.49",
+      "name": "Bagel",
+      "variant": "Onion",
+      "quantity": 1
+    }
+    //WHEN
+    basket.addToBasket(item) 
+    const newRes = basket.removeFromBasket('BGLO')
+    const expectRes = undefined
+    //THEN
+    expect(newRes).toEqual(expectRes)
+  })
+  
 })
