859bac264a36848071602f32041f4a1b8c8aa614
Chris Sach <christophersach90@gmail.com>
2023-06-15 13:56:06 +0100

    core complete

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -23,6 +23,24 @@ describe('Basket', () => {
       }])
   })
 
+  it('Add valid item to basket that is already in', () => {
+    // Given
+    const basket = new Basket()
+    const item = 'BGLO'
+    // When
+    basket.addToBasket(item)
+    const result = basket.addToBasket(item)
+
+    // Then
+    expect(result).toEqual([{
+        sku: "BGLO",
+        price: "0.49",
+        name: "Bagel",
+        variant: "Onion",
+        quantity: 2
+      }])
+  })
+
   it('Add invalid item to basket', () => {
     // Given
     const basket = new Basket()
@@ -91,6 +109,27 @@ describe('Basket', () => {
 
   })
 
+  it('Remove item from basket where quantity is > 1', () => {
+    // Given
+    const basket = new Basket()
+    const item = "BGLO"
+
+    // When
+    basket.addToBasket(item)
+    basket.addToBasket(item)
+
+    const result = basket.removeBasket(item)
+    // Then
+    expect(result).toEqual([{
+      sku: "BGLO",
+      price: "0.49",
+      name: "Bagel",
+      variant: "Onion",
+      quantity: 1
+    }])
+
+  })
+
   it('Add item but basket is full', () => {
     // Given
     const basket = new Basket()
@@ -106,5 +145,93 @@ describe('Basket', () => {
     
     // Then
     expect(result).toEqual("Basket is full")
+  })
+
+  it('Manager increases capacity of basket', () => {
+    // Given
+    const basket = new Basket("Bob")
+
+    // When
+    const result = basket.changeSizeBasket(10)
+
+    // Then
+    expect(result).toEqual("Basket capacity changed to 10")
+  })
+
+  it('User not a manager tries to increase capacity of basket', () => {
+    // Given
+    const basket = new Basket("Tina")
+
+    // When
+    const result = basket.changeSizeBasket(10)
+
+    // Then
+    expect(result).toEqual("Only the manager is authorised to change basket size!")
+  })
+
+  it('User asks for price of item', () => {
+    // Given
+    const basket = new Basket("Tina")
+
+    // When
+    const result = basket.checkPrice("BGLO")
+
+    // Then
+    expect(result).toEqual("The price of the Onion Bagel is £0.49")
+  })
+
+  it('User asks for price of invalid', () => {
+    // Given
+    const basket = new Basket()
+    const item1 = ""
+    const item2 = null
+    const item3 = undefined
+    const item4 = "abc"
+    // When
+    const result1 = basket.checkPrice(item1)
+    const result2 = basket.checkPrice(item2)
+    const result3 = basket.checkPrice(item3)
+    const result4 = basket.checkPrice(item4)
+
+    // Then
+    expect(result1).toEqual('ERROR: Please type in an item')
+    expect(result2).toEqual('ERROR: Please type in an item')
+    expect(result3).toEqual('ERROR: Please type in an item')
+    expect(result4).toEqual('ERROR: Item not found')
+
+  })
+
+  it('User requests total number of bagels in their basket', () => {
+    // Given
+    const basket = new Basket()
+    const item1 = "BGLO"
+    const item2 = "BGLP"
+
+    // When
+    basket.addToBasket(item1)
+    basket.addToBasket(item1)
+    basket.addToBasket(item2)
+    const result = basket.checkNumberBasket()
+
+    // Then
+    expect(result).toEqual('You have 3 bagels in your basket')
+
+
+  })
+
+  it('User requests total cost of bagels in their basket', () => {
+    // Given
+    const basket = new Basket()
+    const item1 = "BGLO"
+    const item2 = "BGLP"
+
+    // When
+    basket.addToBasket(item1)
+    basket.addToBasket(item1)
+    basket.addToBasket(item2)
+    const result = basket.checkBasketPrice()
+
+    // Then
+    expect(result).toEqual('The price of your basket is £1.37')
   })
 })
--- src/basket.js
+++ src/basket.js
@@ -4,19 +4,21 @@ import inventory from '../inventory.json' assert { type: 'json' }
 const store = inventory.inventory
 
 class Basket {
-    constructor() {
+    constructor(user) {
         this.basket = [],
         this.capacity = 5
+        this.user = user
     }
 
-    addToBasket(item) {        
+    addToBasket(item) {
+        // Checks for valid input
         if(item && typeof item !== 'string'){
             return 'ERROR: Please type in an item'
         }
         if(!item) {
             return 'ERROR: Please type in an item'
-
         }
+        // Check that item is in the inventory/store
         const target = store.filter((obj) => obj.sku === item)
         if (target.length === 0){
             return 'ERROR: Item not found'
@@ -27,45 +29,98 @@ class Basket {
         function basketTotal(arr) {
             let itemTotal = 0
             for(let i = 0; i < arr.length; i++) {
-                itemTotal += arr[i].quantity
+                itemTotal += arr[i].quantity                
             }
+            return itemTotal
         }
 
         const baskAmount = basketTotal(this.basket)
         if(baskAmount === this.capacity){
             return "Basket is full"
         }
-        
+
+        // All checks complete, now adding to basket 
         const targObj = target[0]
+        // Check if item is already in basket and add to the quantity
         const baskCheck = this.basket.find(({sku}) => sku === targObj.sku)
         if(baskCheck !== undefined) {
-            const baskIndex = this.basket.indexOf((obj) => obj.sku === item)
+            const baskIndex = this.basket.indexOf(baskCheck)
             this.basket[baskIndex].quantity += 1
             return this.basket
         }
+        // Item is not in basket so add property of quantity = 1 and push to basket
         targObj.quantity = 1
         this.basket.push(targObj)
         return this.basket
     }
 
     removeBasket(item) {
+        // Check for valid input 
         if(item && typeof item !== 'string') {
             return 'ERROR: Please type in an item'
         }
         if(!item) {
             return 'ERROR: Please type in an item'
         }
+        // Check if item exists in store
         const targStore = store.find(({sku}) => sku === item)
         if (targStore === undefined){
             return 'ERROR: Item not found'
         }
+        // Check if item is in basket
         const targ = this.basket.find(({sku}) => sku === item)
         if(targ === undefined) {
             return 'ERROR: Item not in basket'
         }
+        // If item is in basket and quantity is > 1
+        if(targ.quantity > 1){
+            const baskIndex = this.basket.indexOf(targ);
+            this.basket[baskIndex].quantity -= 1
+            return this.basket
+        }
         this.basket = this.basket.filter((obj) => obj.sku !== item)
         return this.basket
     }
+
+    changeSizeBasket(num) {
+        if(this.user !== "Bob") {
+            return "Only the manager is authorised to change basket size!"
+        }
+        this.basket.capacity = num;
+        return `Basket capacity changed to ${num}`
+    }
+
+    checkPrice(item) {
+        // Checks for valid input
+        if(item && typeof item !== 'string'){
+            return 'ERROR: Please type in an item'
+        }
+        if(!item) {
+            return 'ERROR: Please type in an item'
+        }
+        const targStore = store.find(({sku}) => sku === item)
+        if(targStore === undefined) {
+            return 'ERROR: Item not found'
+        }
+        return `The price of the ${targStore.variant} ${targStore.name} is £${targStore.price}`
+    }
+
+    checkNumberBasket() {
+        let numBagels = 0;
+        this.basket.forEach((element) => {
+            numBagels += element.quantity
+        })
+        return `You have ${numBagels} bagels in your basket`
+    }
+
+    checkBasketPrice() {
+        let cost = 0
+        this.basket.forEach((element) => {
+            cost += element.quantity * element.price
+        })
+        return `The price of your basket is £${cost}`
+    }
+
 }
 
 export default Basket
