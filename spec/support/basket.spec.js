fae8fe0b8fc63e59b3b652d53d5e44091ed1388e
Szkaradd <szkaradekmikolaj@gmail.com>
2023-07-20 13:54:53 +0200

    added test for getTotalPrice

--- spec/support/basket.spec.js
+++ spec/support/basket.spec.js
@@ -1,171 +1,200 @@
 const Basket = require('../../src/basket.js')
+const Product = require('../../src/product.js')
+
+describe('Product', () => {
+  let product
+
+  beforeEach(() => {
+    product = new Product('BGLO', '0.49', 'Bagel', 'Onion')
+  })
+
+  it('should have a SKU', () => {
+    expect(product.sku).toEqual('BGLO')
+  })
+
+  it('should have a price', () => {
+    expect(product.price).toEqual('0.49')
+  })
+
+  it('should have a name', () => {
+    expect(product.name).toEqual('Bagel')
+  })
+
+  it('should have a variant', () => {
+    expect(product.variant).toEqual('Onion')
+  })
+
+  it('should return the price', () => {
+    expect(product.getPrice()).toEqual('0.49')
+  })
+})
 
 describe('Basket', () => {
-    let basket;
-    const product1 = {
-        sku: 'BGLO',
-        price: '0.49',
-        name: 'Bagel',
-        variant: 'Onion'
+  let basket
+  const product1 = new Product('BGLO', '0.49', 'Bagel', 'Onion')
+
+  const product2 = new Product('BGLP', '0.39', 'Bagel', 'Plain')
+
+  beforeEach(() => {
+    basket = new Basket(10)
+  })
+
+  it('should be empty when created', () => {
+    expect(basket.isEmpty()).toBe(true)
+  })
+
+  it('should add a new item to the basket', () => {
+    // Execute
+    basket.addItem(product1, 1)
+
+    // Verify
+    expect(basket.contents).toContain(product1)
+    expect(basket.getItemQuantity(product1)).toEqual(1)
+  })
+
+  it('should add multiple items to the basket', () => {
+    // Execute
+    basket.addItem(product1, 6)
+
+    // Verify
+    expect(basket.contents).toContain(product1)
+    expect(basket.getItemQuantity(product1)).toEqual(6)
+  })
+
+  it('should not add an item if the basket is full', () => {
+    // Execute
+    basket.addItem(product1, 10)
+
+    // Verify
+    expect(basket.getItemQuantity(product1)).toEqual(10)
+    expect(() => {
+      basket.addItem(product2, 1)
+    }).toThrowError('Basket is full!!!')
+  })
+
+  it('should not add an item if the quantity is negative', () => {
+    // Verify
+    expect(() => {
+      basket.addItem(product1, -1)
+    }).toThrowError("Can't add negative amount of products to basket")
+  })
+
+  it('should not add an item if the quantity is greater than the capacity', () => {
+    // Verify
+    expect(() => {
+      basket.addItem(product1, 11)
+    }).toThrowError("Can't add as more than 10 items to basket")
+  })
+
+  it('should not add an item if it is not in the inventory', () => {
+    // Setup
+    const product3 = {
+      sku: 'OOOO',
+      price: '0.99',
+      name: 'Onion',
+      variant: 'Onion'
     }
 
-    const product2 = {
-        sku: 'BGLP',
-        price: '0.39',
-        name: 'Bagel',
-        variant: 'Plain'
-    }
+    // Verify
+    expect(() => {
+      basket.addItem(product3, 1)
+    }).toThrowError("Can't add an item that is not in the inventory")
+  })
 
-    beforeEach(() => {
-        basket = new Basket(10)
-    })
+  it('should return true if the basket is full', () => {
+    // Execute
+    basket.addItem(product1, 10)
 
-    it('should be empty when created', () => {
-        expect(basket.isEmpty()).toBe(true)
-    })
+    // Verify
+    expect(basket.isBasketFull()).toBe(true)
+  })
 
-    it('should add a new item to the basket', () => {
-        // Execute
-        basket.addItem(product1, 1)
+  it('should return false if the basket is not full', () => {
+    // Execute
+    basket.addItem(product1, 9)
 
-        // Verify
-        expect(basket.contents).toContain(product1)
-        expect(basket.getItemQuantity(product1)).toEqual(1)
-    })
+    // Verify
+    expect(basket.isBasketFull()).toBe(false)
+  })
 
-    it('should add multiple items to the basket', () => {
-        // Execute
-        basket.addItem(product1, 6)
+  it('should return the number of items in the basket', () => {
+    // Execute
+    basket.addItem(product1, 5)
 
-        // Verify
-        expect(basket.contents).toContain(product1)
-        expect(basket.getItemQuantity(product1)).toEqual(6)
-    })
+    // Verify
+    expect(basket.getItemsCount()).toEqual(5)
+  })
 
-    it('should not add an item if the basket is full', () => {
-        // Execute
-        basket.addItem(product1, 10)
+  it('should return the number of items in the basket for a specific product', () => {
+    // Execute
+    basket.addItem(product1, 5)
+    basket.addItem(product2, 3)
 
-        // Verify
-        expect(basket.getItemQuantity(product1)).toEqual(10)
-        expect(() => {
-            basket.addItem(product2, 1)
-        }).toThrowError("Basket is full!!!")
-    })
+    // Verify
+    expect(basket.getItemQuantity(product1)).toEqual(5)
+    expect(basket.getItemQuantity(product2)).toEqual(3)
+  })
 
-    it('should not add an item if the quantity is negative', () => {
-        // Verify
-        expect(() => {
-            basket.addItem(product1, -1)
-        }).toThrowError("Can't add negative amount of products to basket")
-    })
+  it('should remove an item from the basket', () => {
+    // Setup
+    basket.addItem(product1, 5)
 
-    it('should not add an item if the quantity is greater than the capacity', () => {
-        // Verify
-        expect(() => {
-            basket.addItem(product1, 11)
-        }).toThrowError("Can't add as more than 10 items to basket")
-    })
+    // Execute
+    basket.removeItem(product1, 1)
 
-    it('should not add an item if it is not in the inventory', () => {
-        // Setup
-        const product3 = {
-            sku: 'OOOO',
-            price: '0.99',
-            name: 'Onion',
-            variant: 'Onion'
-        }
+    // Verify
+    expect(basket.getItemQuantity(product1)).toEqual(4)
+  })
 
-        // Verify
-        expect(() => {
-            basket.addItem(product3, 1)
-        }).toThrowError("Can't add an item that is not in the inventory")
-    })
+  it('should remove multiple items from the basket', () => {
+    // Setup
+    basket.addItem(product1, 5)
 
-    it('should return true if the basket is full', () => {
-        // Execute
-        basket.addItem(product1, 10)
+    // Execute
+    basket.removeItem(product1, 3)
 
-        // Verify
-        expect(basket.isBasketFull()).toBe(true)
-    })
+    // Verify
+    expect(basket.getItemQuantity(product1)).toEqual(2)
+  })
 
-    it('should return false if the basket is not full', () => {
-        // Execute
-        basket.addItem(product1, 9)
+  it('should not remove an item from the basket if it is not in the basket', () => {
+    // Setup
+    basket.addItem(product1, 5)
 
-        // Verify
-        expect(basket.isBasketFull()).toBe(false)
-    })
+    // Verify
+    expect(() => {
+      basket.removeItem(product2, 1)
+    }).toThrowError("Can't remove an item that is not in the basket")
+  })
 
-    it('should return the number of items in the basket', () => {
-        // Execute
-        basket.addItem(product1, 5)
+  it('should not remove an item from the basket if the quantity is negative', () => {
+    // Setup
+    basket.addItem(product1, 5)
 
-        // Verify
-        expect(basket.getItemsCount()).toEqual(5)
-    })
+    // Verify
+    expect(() => {
+      basket.removeItem(product1, -1)
+    }).toThrowError("Can't remove negative amount of products from basket")
+  })
 
-    it('should return the number of items in the basket for a specific product', () => {
-        // Execute
-        basket.addItem(product1, 5)
-        basket.addItem(product2, 3)
+  it('should not remove an item from the basket if the quantity is greater than the number of items in the basket', () => {
+    // Setup
+    basket.addItem(product1, 5)
 
-        // Verify
-        expect(basket.getItemQuantity(product1)).toEqual(5)
-        expect(basket.getItemQuantity(product2)).toEqual(3)
-    })
+    // Verify
+    expect(() => {
+      basket.removeItem(product1, 6)
+    }).toThrowError("Can't remove as more than 5 items from basket")
+  })
 
-    it('should remove an item from the basket', () => {
-        // Setup
-        basket.addItem(product1, 5)
+  it('should return the total price of the basket', () => {
+    // Setup
+    basket.addItem(product1, 5)
+    basket.addItem(product2, 3)
 
-        // Execute
-        basket.removeItem(product1, 1)
-
-        // Verify
-        expect(basket.getItemQuantity(product1)).toEqual(4)
-    })
-
-    it('should remove multiple items from the basket', () => {
-        // Setup
-        basket.addItem(product1, 5)
-
-        // Execute
-        basket.removeItem(product1, 3)
-
-        // Verify
-        expect(basket.getItemQuantity(product1)).toEqual(2)
-    })
-
-    it('should not remove an item from the basket if it is not in the basket', () => {
-        // Setup
-        basket.addItem(product1, 5)
-
-        // Verify
-        expect(() => {
-            basket.removeItem(product2, 1)
-        }).toThrowError("Can't remove an item that is not in the basket")
-    })
-
-    it('should not remove an item from the basket if the quantity is negative', () => {
-        // Setup
-        basket.addItem(product1, 5)
-
-        // Verify
-        expect(() => {
-            basket.removeItem(product1, -1)
-        }).toThrowError("Can't remove negative amount of products from basket")
-    })
-
-    it('should not remove an item from the basket if the quantity is greater than the number of items in the basket', () => {
-        // Setup
-        basket.addItem(product1, 5)
-
-        // Verify
-        expect(() => {
-            basket.removeItem(product1, 6)
-        }).toThrowError("Can't remove as more than 5 items from basket")
-    })
-
+    // Verify
+    expect(basket.getTotalPrice()).toEqual(
+      5 * product1.getPrice() + 3 * product2.getPrice()
+    )
+  })
 })
--- src/basket.js
+++ src/basket.js
@@ -1,69 +1,73 @@
-// read inventory from file inventory.json
-
-let data = require('../inventory.json')
-let inventory = data.inventory;
+const data = require('../inventory.json')
+const inventory = data.inventory
 
 class Basket {
-    constructor(capacity) {
-        this.capacity = capacity
-        this.contents = []
+  constructor(capacity) {
+    this.capacity = capacity
+    this.contents = []
+  }
+
+  isEmpty() {
+    return this.contents.length === 0
+  }
+
+  getItemsCount() {
+    return this.contents.length
+  }
+
+  getItemQuantity(product) {
+    return this.contents.filter((item) => item.sku === product.sku).length
+  }
+
+  _getLeftCapacity() {
+    return this.capacity - this.getItemsCount()
+  }
+
+  isBasketFull() {
+    return this._getLeftCapacity() === 0
+  }
+
+  addItem(product, quantity) {
+    if (inventory.findIndex((item) => item.sku === product.sku) === -1) {
+      throw new Error("Can't add an item that is not in the inventory")
+    }
+    if (quantity < 0) {
+      throw new Error("Can't add negative amount of products to basket")
+    }
+    if (this.isBasketFull()) {
+      throw new Error('Basket is full!!!')
+    }
+    if (this._getLeftCapacity() - quantity < 0) {
+      throw new Error(
+        `Can't add as more than ${this._getLeftCapacity()} items to basket`
+      )
     }
 
-    isEmpty() {
-        return this.contents.length === 0
+    for (let i = 0; i < quantity; i++) {
+      this.contents.push(product)
+    }
+  }
+
+  removeItem(product, quantity) {
+    if (quantity < 0) {
+      throw new Error("Can't remove negative amount of products from basket")
+    }
+    if (this.getItemQuantity(product) === 0) {
+      throw new Error("Can't remove an item that is not in the basket")
+    }
+    if (this.getItemQuantity(product) - quantity < 0) {
+      throw new Error(
+        `Can't remove as more than ${this.getItemQuantity(
+          product
+        )} items from basket`
+      )
     }
 
-    getItemsCount() {
-        return this.contents.length
+    for (let i = 0; i < quantity; i++) {
+      const index = this.contents.findIndex((item) => item.sku === product.sku)
+      this.contents.splice(index, 1)
     }
-
-    getItemQuantity(product) {
-        return this.contents.filter(item => item.sku === product.sku).length
-    }
-
-    _getLeftCapacity() {
-        return this.capacity - this.getItemsCount()
-    }
-
-    isBasketFull() {
-        return this._getLeftCapacity() === 0
-    }
-
-    addItem(product, quantity) {
-        if (inventory.findIndex(item => item.sku === product.sku) === -1) {
-            throw new Error("Can't add an item that is not in the inventory")
-        }
-        if (quantity < 0) {
-            throw new Error("Can't add negative amount of products to basket")
-        }
-        if (this.isBasketFull()) {
-            throw new Error("Basket is full!!!")
-        }
-        if (this._getLeftCapacity() - quantity < 0) {
-            throw new Error(`Can't add as more than ${this._getLeftCapacity()} items to basket`)
-        }
-
-        for (let i = 0; i < quantity; i++) {
-            this.contents.push(product)
-        }
-    }
-
-    removeItem(product, quantity) {
-        if (quantity < 0) {
-            throw new Error("Can't remove negative amount of products from basket")
-        }
-        if (this.getItemQuantity(product) === 0) {
-            throw new Error("Can't remove an item that is not in the basket")
-        }
-        if (this.getItemQuantity(product) - quantity < 0) {
-            throw new Error(`Can't remove as more than ${this.getItemQuantity(product)} items from basket`)
-        }
-
-        for (let i = 0; i < quantity; i++) {
-            const index = this.contents.findIndex(item => item.sku === product.sku)
-            this.contents.splice(index, 1)
-        }
-    }
+  }
 }
 
 module.exports = Basket
