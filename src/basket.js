c70ce6fedfdbef420269e697edc1f80aeb33d7e0
Ewa Skorek <37912084+akaaritah@users.noreply.github.com>
2023-07-21 00:32:57 +0200

    some edits
    core working, extension still not working

--- src/basket.js
+++ src/basket.js
@@ -13,44 +13,45 @@ class Basket {
             console.log('Basket is full!')
             return false
         }
-    
+
         const existingItem = this.items.find((item) => item.sku === sku)
         if (existingItem) {
             existingItem.quantity++
             existingItem.price = (
                 Math.round(Number(existingItem.price) * existingItem.quantity * 100) / 100
-            ).toFixed(2)
+            ).toString()
             return this.items
         }
-    
+
         const newItem = inventory.find((item) => item.sku === sku)
         if (!newItem) {
             console.log('Item not in stock!')
             return false
         }
-    
+
         newItem.quantity = 1
         this.items.push({ ...newItem })
         return this.items
     }
 
     setCapacity(capacity) {
-        if(capacity < this.currentAmount()) {
+        if (capacity < this.currentAmount()) {
             return false
         }
         this.capacity = capacity
-        return this.capacity    
+        return this.capacity
     }
 
-    removeItem(Sku) {
-        const Item = this.items.find(({ sku }) => sku === Sku)
-        if (!Item) {
+    removeItem(sku) {
+        const itemIndex = this.items.findIndex((item) => item.sku === sku)
+        if (itemIndex === -1) {
             console.log('Item is not in the basket!')
             return false
         }
-        Item.quantity -= 1
-        if (Item.quantity <= 0) {
-            this.items.splice(this.items.indexOf(Item), 1)
+        const item = this.items[itemIndex]
+        item.quantity--
+        if (item.quantity <= 0) {
+            this.items.splice(itemIndex, 1)
         }
         return this.items
     }
@@ -59,7 +60,7 @@ class Basket {
         let currentAmount = 0
         this.items.forEach((item) => {
             currentAmount += item.quantity
-        })
+        });
         return currentAmount
     }
 
@@ -68,59 +69,60 @@ class Basket {
     }
 
     getDiscount() {
-  let countCoffee = 0
-  let restofBagels = 0
-  let restofCoffee = 0
+        let countCoffee = 0
+        let restofBGLP = 0
+        let restofCoffee = 0
+        this.items.forEach((item) => {
+            if (item.sku === 'COF') {
+                countCoffee = item.quantity
+            }
+            if (item.sku === 'BGLP') {
+                restofBGLP = item.quantity
+            }
+        })
 
-  this.items.forEach((item) => {
-    if (item.sku === 'COF') {
-      countCoffee = item.quantity
-    }
-    if (item.sku === 'BGLP') {
-      restofBagels = item.quantity % 6
-    }
-  })
+        this.items.forEach((item) => {
+            if (item.sku === 'BGLO' || item.sku === 'BGLE') {
+                const discount = Math.floor(item.quantity / 6) * 2.49
+                const reminder = (item.quantity % 6) * 0.49
+                item.price = (discount + reminder).toFixed(2)
+            }
+            if (item.sku === 'BGLP') {
+                const discount = Math.floor(item.quantity / 12) * 3.99
 
-  this.items.forEach((item) => {
-    if (item.sku === 'BGLO' || item.sku === 'BGLE') {
-      const discount = Math.floor(item.quantity / 6) * 2.49
-      const rest = (item.quantity % 6) * 0.49
-      item.price = (discount + rest).toFixed(2)
+                let reminder = 0
+                if (restofBGLP) {
+                    const restofPlain = Math.abs(countCoffee - restofBGLP)
+                    reminder = restofPlain * 0.39
+                }
+                item.price = (discount + reminder).toFixed(2)
+            }
+            if (item.sku === 'COF') {
+                const coffeePriceDiscount =
+                    restofBGLP % countCoffee > 0
+                        ? (restofBGLP % countCoffee) * 1.25
+                        : restofBGLP === 1 && countCoffee === 1
+                            ? 1.25
+                            : 0
+                if (countCoffee - restofBGLP > 0) {
+                    restofCoffee = (countCoffee - restofBGLP) * 0.99
+                }
+
+                item.price = (coffeePriceDiscount + restofCoffee).toFixed(2)
+            }
+        });
+        return this.items
     }
-    if (item.sku === 'BGLP') {
-      const discount = Math.floor(item.quantity / 12) * 3.99
-      let rest = 0
-      if (restofBagels) {
-        const restOfPlain =
-          countCoffee > restofBagels ? countCoffee - restofBagels : restofBagels - countCoffee;
-        rest = restOfPlain * 0.39
-      }
-      item.price = (discount + rest).toFixed(2)
-    }
-    if (item.sku === 'COF') {
-      const coffeeDiscount =
-        restofBagels % countCoffee > 0
-          ? (restofBagels % countCoffee) * 1.25
-          : restofBagels === 1 && countCoffee === 1
-          ? 1.25
-          : 0
-      if (countCoffee - restofBagels > 0) {
-        restofCoffee = (countCoffee - restofBagels) * 0.99
-      }
-      item.price = (coffeeDiscount + restofCoffee).toFixed(2)
-    }
-  })
-}
-      
-      getTotalPrice() {
+
+    getTotalPrice() {
         this.totalPrice = 0
-        for (const position of this.items) {
-          this.totalPrice += Number(position.price) * position.quantity
-        }
-        return this.totalPrice
-      }
+        this.items.forEach((item) => {
+            this.totalPrice += Number(item.price)
+        });
+        return this.totalPrice.toFixed(2)
+    }
 }
 
 module.exports = {
-    Basket: Basket
-}
+    Basket: Basket,
+};
