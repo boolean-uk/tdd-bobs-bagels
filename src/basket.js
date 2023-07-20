52ec32c97974a4d1dcb3308d8946128d4288d661
Marcin Stefanowicz <marcin.stefanowicz@talan.com>
2023-07-20 13:48:11 +0200

    red stage of add

--- src/basket.js
+++ src/basket.js
@@ -10,12 +10,24 @@ class Basket {
   }
 
   add(bagel, quantity) {
+    const findResult = this.find(bagel);
+
+    if (findResult.length > 0) {
+      findResult[0].quantity += quantity;
+
+      return true;
+    }
+
     this.bagels.push({
       bagel,
       quantity,
     });
 
     return true;
+  }
+
+  find(bagel) {
+    return this.bagels.filter((item) => item.bagel.name === bagel.name);
   }
 }
 
