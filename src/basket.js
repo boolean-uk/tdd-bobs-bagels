64faff6262b08847e790ec8f033bd542a12360c0
DorotaWlazlo <dori.wlazlo@gmail.com>
2023-07-20 12:33:14 +0200

    created adding and removing functions

---
+++ src/basket.js
@@ -0,0 +1,42 @@
+// eslint-disable-next-line no-unused-vars
+const bagelsInBasket = {}
+const capacity = 10
+let currentAmount = 0
+const json = require('../inventory.json')
+const inventory = json.inventory
+
+initializeBasket(inventory)
+function initializeBasket(inventory) {
+  for (const i in inventory) {
+    bagelsInBasket[inventory[i].sku] = 0
+  }
+}
+
+function add(bagelSku, amount) {
+  if (
+    amount <= 0 ||
+    amount + currentAmount > capacity ||
+    bagelsInBasket[bagelSku] === undefined
+  )
+    return false
+  bagelsInBasket[bagelSku] += amount
+  currentAmount += amount
+  return true
+}
+
+function remove(bagelSku, amount) {
+  if (
+    amount <= 0 ||
+    bagelsInBasket[bagelSku] === undefined ||
+    bagelsInBasket[bagelSku] < amount
+  )
+    return false
+  bagelsInBasket[bagelSku] -= amount
+  currentAmount -= amount
+  return true
+}
+
+module.exports = {
+  add,
+  remove
+}
