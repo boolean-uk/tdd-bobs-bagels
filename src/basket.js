12bea098abc7c3adc57b1ac446de604169442926
Martyna <martyna.marszalek@talan.com>
2023-07-20 16:28:59 +0200

    implementation of generating receipt

--- src/basket.js
+++ src/basket.js
@@ -47,5 +47,47 @@ class Basket {
     })
     return total
   }
+
+  generateReceipt() {
+    const now = new Date().toLocaleString()
+    let receipt = `     ~~~ Bob's Bagels ~~~\n\n    ${now}\n\n-----------------------------\n`
+
+    const itemTotals = {}
+    this.items.forEach((itemSku) => {
+      const item = inv.find((invItem) => invItem.sku === itemSku)
+      if (item) {
+        itemTotals[itemSku] = itemTotals[itemSku] ? itemTotals[itemSku] + 1 : 1
+      }
+    })
+
+    let maxItemLength = 0
+    let maxVariantLength = 0
+    Object.keys(itemTotals).forEach((itemSku) => {
+      const item = inv.find((invItem) => invItem.sku === itemSku)
+      if (item) {
+        maxItemLength = Math.max(maxItemLength, item.name.length)
+        maxVariantLength = Math.max(maxVariantLength, item.variant.length)
+      }
+    })
+
+    Object.keys(itemTotals).forEach((itemSku) => {
+      const item = inv.find((invItem) => invItem.sku === itemSku)
+      if (item) {
+        const itemTotalPrice = (item.price * itemTotals[itemSku]).toFixed(2)
+        const quantity = itemTotals[itemSku].toString().padStart(4)
+        const itemName = item.name.padEnd(maxItemLength + 1)
+        const variant = item.variant.padEnd(maxVariantLength)
+        receipt += `${itemName} ${variant} ${quantity}  £${itemTotalPrice}\n`
+      }
+    })
+
+    receipt += '\n-----------------------------\n'
+
+    const totalPrice = this.calculateTotalPrice().toFixed(2)
+    receipt += `Total\t\t\t£${totalPrice}\n\n`
+    receipt += '        Thank you\n     for your order!'
+
+    return receipt
+  }
 }
 module.exports = Basket
