14ce96281de243651449fb1b8ca045d724e7ab49
Marcin Stefanowicz <marcin.stefanowicz@talan.com>
2023-07-20 15:09:32 +0200

    red stage of isFull method

--- spec/support/basket.spec.js
+++ spec/support/basket.spec.js
@@ -67,14 +67,13 @@ describe("basket class tests", () => {
     basket.add(newBagel, 4);
     const result = basket.remove(newBagel, 2);
 
-    const resultQuantity= basket.bagels.filter(
+    const resultQuantity = basket.bagels.filter(
       (item) => item.bagel.name === "Plain"
     )[0].quantity;
 
     expect(result).toEqual(true);
     expect(resultQuantity).toEqual(2);
-
-  })
+  });
 
   it("should not remove if bagel with given name does not exist", () => {
     const newBagel = new Bagel("Plain", 3.57);
@@ -85,17 +84,16 @@ describe("basket class tests", () => {
     const result = basket.remove(invalidBagel, 2);
 
     expect(result).toEqual(false);
-
-  })
+  });
 
   it("should not remove bagel if given quantity is invalid", () => {
     const newBagel = new Bagel("Plain", 3.57);
     const basket = new Basket();
 
-    basket.add(newBagel, 1)
+    basket.add(newBagel, 1);
     const result = basket.remove(newBagel, -4);
 
-    const resultQuantity= basket.bagels.filter(
+    const resultQuantity = basket.bagels.filter(
       (item) => item.bagel.name === "Plain"
     )[0].quantity;
 
@@ -118,6 +116,23 @@ describe("basket class tests", () => {
     expect(resultLength).toEqual(0);
   });
 
+  it("should return true if basket is full when given quantity will be added", () => {
+    const newBagel = new Bagel("Plain", 3.57);
+    const basket = new Basket();
 
+    basket.add(newBagel, 8);
+    const result = basket.isFull(3);
 
+    expect(result).toEqual(true);
+  });
+
+  it("should return false if basket is not full when given quantity will be added", () => {
+    const newBagel = new Bagel("Plain", 3.57);
+    const basket = new Basket();
+
+    basket.add(newBagel, 8);
+    const result = basket.isFull(2);
+
+    expect(result).toEqual(false);
+  });
 });
