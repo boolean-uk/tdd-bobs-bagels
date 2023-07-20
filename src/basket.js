ec28eb837f9e299b47b16397979182f79b6e47f5
Michał Klukowski <michal.klukowski@talan.com>
2023-07-20 12:42:32 +0200

    Implement removing item from basket

--- src/basket.js
+++ src/basket.js
@@ -2,6 +2,12 @@ function addItem(basket, item) {
     basket.items.push(item)
 }
 
+function removeItem(basket, item) {
+    const index = basket.items.indexOf(item)
+    basket.items.splice(index, 1)
+}
+
 module.exports = {
-    addItem
+    addItem,
+    removeItem
 }
