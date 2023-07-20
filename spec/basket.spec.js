e0cbe82e6fcf33b90d74d57e908ac1fb6136e61e
Michal Antoniuk <michal.antoniuk@talan.com>
2023-07-20 12:53:42 +0200

    Add test for removing item from basket

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -13,11 +13,21 @@ describe("Basket tests", () => {
 
   it('shoud add item to basket', () => {
     // Setup
-    const item = {name: 'item', price: 19.99}
+    const item = {name: 'item', unitPrice: 19.99}
     basket.addItem(item)
 
     // Test
     expect(basket.items.length).toEqual(1)
+  })
+
+  it('should remove item from basket', () => {
+    // Setup
+    const item = {name: 'item', price: 19.99}
+    basket.addItem(item)
+    basket.remove(item)
+
+    // Test
+    expect(basket.items.length).toEqual(0)
   })
 
 }) 
