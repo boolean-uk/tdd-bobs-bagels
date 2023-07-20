c833ec1e28820b7a87c1544698d11b804c7fde23
Przemyslaw Mroczek <przemyslaw.mroczek@talan.com>
2023-07-20 16:11:50 +0200

    Extension 2 add receipt

--- spec/basket.extension.spec.js
+++ spec/basket.extension.spec.js
@@ -54,4 +54,31 @@ describe('Basket test - extension', () => {
 
     expect(basket.getTotal()).toEqual(9.12)
   })
+
+  it('should prepare summary', function () {
+    const basket = new Basket(30)
+    const bagel1 = new Product('BGLO', 'Onion', 0.49)
+    const bagel2 = new Product('BGLP', 'Plain', 0.39)
+    const coffee = new Product('COFW', 'White', 0.99)
+
+    basket.addProducts(bagel1, 6)
+    basket.addProducts(bagel2, 12)
+    basket.addProducts(bagel2, 3)
+    basket.addProducts(bagel1, 3)
+    basket.addProducts(coffee, 2)
+
+    console.log(basket.generateReceipt())
+  })
+
+  it('should return proper date format', function () {
+    const date = Basket.getCurrentDateTime(new Date('2022-06-17T11:06:50.369Z'))
+
+    expect(date).toEqual('2022-06-17 13:06:50')
+  })
+
+  it('should throw error if arg is not a date', function () {
+    expect(() =>
+      Basket.getCurrentDateTime('2022-06-17T11:06:50.369Z')
+    ).toThrowError("It's not a date")
+  })
 })
--- src/basket.js
+++ src/basket.js
@@ -1,4 +1,5 @@
 const Inventory = require('./inventory')
+
 class Basket {
   constructor(capacity) {
     if (capacity <= 0) {
@@ -7,6 +8,7 @@ class Basket {
       this.capacity = capacity
     }
     this.products = []
+    this.summary = []
   }
 
   addProduct(bagel) {
@@ -34,38 +36,98 @@ class Basket {
     this.capacity = newCapacity
   }
 
+  prepareSummary() {
+    this.summary = []
+    this.products.forEach((prod) => {
+      if (!this.summary[prod.sku]) {
+        this.summary[prod.sku] = {
+          type: prod.type,
+          price: prod.price,
+          quantity: 1,
+          value: 0
+        }
+      } else {
+        this.summary[prod.sku].quantity++
+      }
+    })
+  }
+
   getTotal() {
     const inventory = new Inventory().getAvailableItems()
 
-    const summary = {}
-    this.products.forEach((prod) => {
-      if (!summary[prod.sku]) {
-        summary[prod.sku] = {
-          price: prod.price,
-          quantity: 1
-        }
-      } else {
-        summary[prod.sku].quantity++
-      }
-    })
+    this.prepareSummary()
 
     let total = 0
-    for (const sku in summary) {
+    for (const sku in this.summary) {
       const product = inventory.find((p) => p.sku === sku)
-      const { price, quantity } = summary[sku]
+      const { price, quantity } = this.summary[sku]
 
       if (product && product.specialOffer) {
         const { quantity: offerQuantity, price: offerPrice } =
           product.specialOffer
         const offerSets = Math.floor(quantity / offerQuantity)
         const remainingItems = quantity % offerQuantity
-        total += offerSets * offerPrice + remainingItems * price
+        const itemsValue = offerSets * offerPrice + remainingItems * price
+        this.summary[sku].value = itemsValue
+        total += itemsValue
       } else {
-        total += quantity * price
+        const itemsValue = quantity * price
+        this.summary[sku].value = itemsValue
+
+        total += itemsValue
       }
     }
 
     return Math.round(total * 100) / 100
+  }
+
+  generateReceipt() {
+    this.prepareSummary()
+
+    const header = `\n    ~~~ Bob's Bagels ~~~\n`
+    const time = `\n    ${Basket.getCurrentDateTime(new Date())}\n\n`
+    const spacer = `----------------------------\n`
+    const footer = `\n\n         Thank you\n      for your order!`
+    const total = `Total`.padEnd(17) + `£${this.getTotal()}`.padStart(11)
+    let content = ``
+
+    for (const sku in this.summary) {
+      const { type, quantity, value } = this.summary[sku]
+
+      const name = this.getName(sku, type)
+      content += `${name.padEnd(17)}${quantity
+        .toString()
+        .padStart(3)}   £${value.toFixed(2)}\n`
+    }
+
+    return header + time + spacer + content + spacer + total + footer
+  }
+
+  getName(sku, type) {
+    switch (sku.charAt(0)) {
+      case 'B':
+        return type + ' Bagel'
+      case 'C':
+        return 'Coffee'
+      default:
+        return type
+    }
+  }
+
+  static getCurrentDateTime(date) {
+    if (!(date instanceof Date)) {
+      throw new Error("It's not a date")
+    }
+
+    const year = date.getFullYear()
+    const month = String(date.getMonth() + 1).padStart(2, '0')
+    const day = String(date.getDate()).padStart(2, '0')
+
+    const hours = String(date.getHours()).padStart(2, '0')
+    const minutes = String(date.getMinutes()).padStart(2, '0')
+    const seconds = String(date.getSeconds()).padStart(2, '0')
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
   }
 }
 
