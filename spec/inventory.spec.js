37655067b31dc55eda5445b493f57d19c7feeff8
Przemyslaw Mroczek <przemyslaw.mroczek@talan.com>
2023-07-20 13:30:53 +0200

    Get discount - red test

--- spec/inventory.spec.js
+++ spec/inventory.spec.js
@@ -1,11 +1,20 @@
 const Inventory = require('../src/inventory')
 const Product = require('../src/product')
+const Discount = require('../src/Discount')
 describe('inventory test - core', () => {
-  it('should display available bagels with prices', function () {
+  it('should display available products with prices', function () {
     const inventory = new Inventory()
 
     const items = inventory.getAvailableItems()
     expect(items.length).toEqual(4)
     expect(items.filter((prod) => prod instanceof Product))
+  })
+
+  it('should display available discounts', function () {
+    const inventory = new Inventory()
+
+    const discounts = inventory.getAvailableDiscounts()
+    expect(discounts.length).toEqual(3)
+    expect(discounts.filter((prod) => prod instanceof Discount))
   })
 })
