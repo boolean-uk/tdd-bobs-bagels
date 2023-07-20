f74249b09c6e1aa5cf2073d2dd7b3e0ae68f2590
Martyna <martyna.marszalek@talan.com>
2023-07-20 13:59:59 +0200

    implementation for method removeItem

--- src/basket.js
+++ src/basket.js
@@ -21,5 +21,19 @@ class Basket {
 
     return this.items
   }
+
+  removeItem(name) {
+    //check if item exists
+    const item = this.items.find((item) => item.name === name)
+    if (!item) {
+      return 'Item does not exist'
+    }
+    //decrease the quantity
+    item.quantity -= 1
+    if (item.quantity <= 0) {
+      this.items.splice(this.items.indexOf(item), 1)
+    }
+    return this.items
+  }
 }
 module.exports = Basket
