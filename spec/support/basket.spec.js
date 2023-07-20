66a3d169621fc3738c1bcdd9c36067cab0958059
Rafal Halama <rafau.halama@gmail.com>
2023-07-20 12:08:01 +0200

    tests for add

---
+++ spec/support/basket.spec.js
@@ -0,0 +1,21 @@
+const {} = require('../src/basket')
+
+describe('basket tests', () => {
+  let basket
+  beforeEach(() => {
+    basket = new Basket()
+  })
+
+  it('bagel added to array', () => {
+    expect(basket.add('BGLP')).toEqual({
+      sku: 'BGLP',
+      price: '0.39',
+      name: 'Bagel',
+      variant: 'Plain'
+    })
+  })
+
+  it('bagel not added to array', () => {
+    expect(basket.add('NONEXISTING')).toEqual([])
+  })
+})
