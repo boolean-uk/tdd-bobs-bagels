87fd7792aa5b47f104647951fddd72767a62178d
Przemyslaw Mroczek <przemyslaw.mroczek@talan.com>
2023-07-20 12:22:05 +0200

    Remove bagel - test red

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -15,4 +15,12 @@ describe('Basket test - core', () => {
     const bagel = new Bagel('plain', 0.49)
     expect(() => basket.addBagel(bagel)).toThrow(new Error('Capacity exceeded'))
   })
+
+  it('should remove bagel from basket', function () {
+    const basket = new Basket(5)
+    const bagel = new Bagel('plain', 0.49)
+    basket.removeBagel(bagel)
+
+    expect(basket.products.length).toEqual(0)
+  })
 })
