0ed8c6358406e86c3aa99a022f25add4ba2d5e90
Michał Klukowski <michal.klukowski@talan.com>
2023-07-20 12:42:19 +0200

    Create test for removing an item from basket

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-const { addItem } = require("../src/basket")
+const { addItem, removeItem } = require("../src/basket")
 
 describe('Simple basket operations', () => {
     
@@ -21,5 +21,18 @@ describe('Simple basket operations', () => {
     it('should add an item to basket', () => {
         addItem(basket, inventory[0])
         expect(basket.items.length).toEqual(1)
+    })
+
+    it('should remove the item from basket', () => {
+        const itemToRemove = inventory[1]
+
+        addItem(basket, inventory[0])
+        addItem(basket, inventory[1])
+        addItem(basket, inventory[2])
+
+        removeItem(basket, itemToRemove)
+        
+        expect(basket.items.length).toEqual(2)
+        expect(basket.items.indexOf(itemToRemove)).toEqual(-1)
     })
 })
