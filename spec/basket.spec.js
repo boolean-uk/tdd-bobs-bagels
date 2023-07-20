fc534496b0e375cfdc54db3515c4504f9bfa24a2
Martyna <martyna.marszalek@talan.com>
2023-07-20 14:07:56 +0200

    Adding tests to basket capacity

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -12,6 +12,24 @@ describe('addItem', () => {
   })
 })
 
+describe('addItem', () => {
+  const basket = new Basket(3)
+  it('should add 2 items to the basket', () => {
+    basket.addItem('BG')
+    basket.addItem('BH')
+    expect(basket.items.length).toEqual(2)
+  })
+})
+
+describe('addItem', () => {
+  const basket = new Basket(0)
+  it('shouldnot add any item to the basket', () => {
+    const item = basket.addItem('BG')
+    expect(basket.items.length).toEqual(0)
+    expect(item).toEqual('You can not add an item')
+  })
+})
+
 describe('removeItem', () => {
   const basket = new Basket(2)
   it('should remove item from the basket', () => {
