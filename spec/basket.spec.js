e576a84a11396d7864eacb87ffcf7c34a7ae960b
Michal Antoniuk <michal.antoniuk@talan.com>
2023-07-23 23:34:27 +0200

    Add test for extension 3

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -1,4 +1,5 @@
 const { Basket }= require('../src/basket.js')
+const placeOrder = require('../src/order.js')
 
 describe("Basket tests", () => {
   let basket;
@@ -127,25 +128,55 @@ describe("Basket tests", () => {
     basket.addItem("BGSS")
 
     //Test
-    expect(basket.getTotalPrice()).toEqual(5.48)
+    expect(basket.getTotalPrice()).toEqual(7.48)
   })
 
   it('should print receipt', () => {
     //Setup
     basket = new Basket(30)
-    Array(2).fill().forEach(() => basket.addItem("BGLO"));
-    Array(12).fill().forEach(() => basket.addItem("BGLP"));
-    Array(3).fill().forEach(() => basket.addItem("BGLE"));
-    Array(3).fill().forEach(() => basket.addItem("COF"));
-    const receipt = basket.placeOrder()
+    Array(2).fill().forEach(() => basket.addItem("BGLO"))
+    Array(12).fill().forEach(() => basket.addItem("BGLP"))
+    Array(3).fill().forEach(() => basket.addItem("BGLE"))
+    Array(3).fill().forEach(() => basket.addItem("COF"))
+    const [receipt, orderJson] = placeOrder(basket.items)
+    const dateOfOrder = JSON.parse(orderJson)['Date of order']
 
     //Test
     expect(receipt).toContain("~~~ Bob's Bagels ~~~")
-    expect(receipt).toContain("--------------------------------")
-    expect(receipt).toContain("Bagel   Onion      2  £0.98")
-    expect(receipt).toContain("Bagel   Plain      12  £4.68")
-    expect(receipt).toContain("Bagel   Everything 3  £1.47")
-    expect(receipt).toContain("Coffee             3  £2.97")
-    expect(receipt).toContain("Total                    £8.72")
+    expect(receipt).toContain(dateOfOrder)
+    expect(receipt).toContain("------------------")
+    expect(receipt).toMatch("Bagel.*Onion.*2.*£0.98")
+    expect(receipt).toMatch("Bagel.*Plain.*12.*£4.68")
+    expect(receipt).toMatch("Bagel.*Everything.*3.*£1.47")
+    expect(receipt).toMatch("Coffee.*3.*£2.97")
+    expect(receipt).toMatch("Total.*10.10")
+  })
+
+  it('should print savings', () => {
+    //Setup
+    basket = new Basket(30)
+    Array(2).fill().forEach(() => basket.addItem("BGLO"))
+    Array(12).fill().forEach(() => basket.addItem("BGLP"))
+    Array(6).fill().forEach(() => basket.addItem("BGLE"))
+    Array(3).fill().forEach(() => basket.addItem("COF"))
+    const [receipt, orderJson] = placeOrder(basket.items)
+    const dateOfOrder = JSON.parse(orderJson)['Date of order']
+
+    //Test
+    expect(receipt).toContain("~~~ Bob's Bagels ~~~")
+    expect(receipt).toContain(dateOfOrder)
+    expect(receipt).toContain("------------------")
+    expect(receipt).toMatch("Bagel.*Onion.*2.*£0.98")
+    expect(receipt).toContain("(-£0.49)")
+    expect(receipt).toMatch("Bagel.*Plain.*12.*£4.68")
+    expect(receipt).toContain("(-£0.69)")
+    expect(receipt).toMatch("Bagel.*Everything.*6.*£2.94")
+    expect(receipt).toContain("(-£0.49)")
+    expect(receipt).toMatch("Coffee.*3.*£2.97")
+    expect(receipt).toMatch("Total.*11.57")
+    expect(receipt).toContain("You saved a total of £1.14")
+    expect(receipt).toContain("on this shop")
+    expect(receipt).toContain("Thank you")
+    expect(receipt).toContain("for your order!")
   })
 })
