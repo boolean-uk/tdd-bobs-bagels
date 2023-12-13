88545e1adc183b32d276d75b25eff13bddfe7bcf
Satoki <satokiito@gmail.com>
2023-12-13 13:17:52 +0000

    part 1 add bagel source code written

--- src/basket.js
+++ src/basket.js
@@ -1,8 +1,32 @@
-class Basket {
-    constructor() {
-        this.basketList = []
-    }
+const { bagelsInventory } = require('../inventory.json')
 
+class Basket {
+  constructor() {
+    this.basketList = []
+  }
+
+  getBasket() {
+    if (this.basketList.length === 0) {
+      return []
+    } else {
+      return this.basketList
+    }
+  }
+
+  addBagel(bagelType) {
+    const bagelToAdd = bagelsInventory.find((bagel) => bagel.sku === bagelType)
+    if (!bagelToAdd) {
+      return 'bagel does not exist, check bagel sku'
+    } else {
+      this.basketList.push(bagelToAdd)
+      return bagelToAdd
+    }
+  }
 }
 
-module.exports = { Basket } 
+const b = new Basket()
+// b.addBagel('BGSS')
+console.log(b.addBagel('BGSS'))
+console.log(b.getBasket())
+
+module.exports = { Basket }
