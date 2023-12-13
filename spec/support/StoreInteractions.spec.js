7945776db46b0e50c5ec522c8b7afb6307a2c8ea
Rebecca Noy <rebecca.noy@gmx.de>
2023-12-13 17:01:41 +0100

    add criteria of name match to test

--- spec/support/StoreInteractions.spec.js
+++ spec/support/StoreInteractions.spec.js
@@ -4,5 +4,6 @@ describe("Store interactions", () => {
   const myStore = new store("Bob's Bagels")
   it("initially, the store exists but has no available bucket types", () => {
     expect(myStore.availableBasketTypes).toEqual([])
+    expect(myStore.name === "Bob's Bagels").toBeTrue()
   })
 })
