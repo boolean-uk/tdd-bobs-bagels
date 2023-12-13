4b4e7b4b1a5b3e15039c3d92a5ef6668c4dff23f
callumhayden <callum.hayden@gmail.com>
2023-12-13 13:18:14 +0000

    ive simplified my code so i wasnt constantly repeating myself. All test's still work properly

--- spec/Basket.spec.js
+++ spec/Basket.spec.js
@@ -2,112 +2,35 @@ const BagelBasket = require('../src/basket.js')
 
 describe('BagelBasket', () => {
   let basket
-  const bagel1 = {
-    sku: 'BGLO',
-    price: '0.49',
-    name: 'Bagel',
-    variant: 'Onion'
-  }
-  const bagel2 = {
-    sku: 'BGLP',
-    price: '0.39',
-    name: 'Bagel',
-    variant: 'plain'
-  }
-  const bagel3 = {
-    sku: 'BGLE',
-    price: '0.49',
-    name: 'Bagel',
-    variant: 'Everything'
-  }
-  const bagel4 = {
-    sku: 'BGLS',
-    price: '0.49',
-    name: 'Bagel',
-    variant: 'Sesame'
-  }
-  const bagel5 = {
-    sku: 'COF',
-    price: '0.39',
-    name: 'Coffee',
-    variant: ''
-  }
-  const bagel6 = {
-    sku: 'BGSE',
-    price: '2.99',
-    name: 'Bagel Sandwich',
-    variant: 'Everything'
-  }
-  const bagel7 = {
-    sku: 'BGSS',
-    price: '4.99',
-    name: 'Bagel',
-    variant: 'Sesame'
-  }
+  const bagels = [
+    { sku: 'BGLO', price: '0.49', name: 'Bagel', variant: 'Onion' },
+    { sku: 'BGLP', price: '0.39', name: 'Bagel', variant: 'Plain' },
+    { sku: 'BGLE', price: '0.49', name: 'Bagel', variant: 'Everything' },
+    { sku: 'BGLS', price: '0.49', name: 'Bagel', variant: 'Sesame' },
+    { sku: 'COF', price: '0.39', name: 'Coffee', variant: '' },
+    { sku: 'BGSE', price: '2.99', name: 'Bagel Sandwich', variant: 'Everything'},
+    { sku: 'BGSS', price: '4.99', name: 'Bagel', variant: 'Sesame' }
+  ]
+
   beforeEach(() => {
     basket = new BagelBasket()
   })
-//bagel 1 add and remove//
-  it('Add item to basket', () => {
-    basket.addItem(bagel1)
-    expect(basket.items).toContain(bagel1)
+
+  bagels.forEach((bagel, index) => {
+    describe(`Bagel ${index + 1}`, () => {
+      it('adds and removes an item from the basket', () => {
+        basket.addItem(bagel)
+        expect(basket.items).toContain(bagel)
+        basket.removeItem(bagel)
+        expect(basket.items).not.toContain(bagel)
+      })
+    })
   })
-  it('Remove item from basket', () => {
-    basket.removeItem(bagel1)
-    expect(basket.items).not.toContain(bagel1)
-  })
-  //bagel 2 add and remove//
-  it('Add item to basket', () => {
-    basket.addItem(bagel2)
-    expect(basket.items).toContain(bagel2)
-  })
-  it('Remove item from basket', () => {
-    basket.removeItem(bagel2)
-    expect(basket.items).not.toContain(bagel2)
-  })
-  //bagel 3 add and remove//
-  it('Add item to basket', () => {
-    basket.addItem(bagel3)
-    expect(basket.items).toContain(bagel3)
-  })
-  it('Remove item from basket', () => {
-    basket.removeItem(bagel3)
-    expect(basket.items).not.toContain(bagel3)
-  })
-  //bagel 4 add and remove//
-  it('Add item to basket', () => {
-    basket.addItem(bagel4)
-    expect(basket.items).toContain(bagel4)
-  })
-  it('Remove item from basket', () => {
-    basket.removeItem(bagel4)
-    expect(basket.items).not.toContain(bagel4)
-  })
-  //bagel 5 add and remove//
-  it('Add item to basket', () => {
-    basket.addItem(bagel5)
-    expect(basket.items).toContain(bagel5)
-  })
-  it('Remove item from basket', () => {
-    basket.removeItem(bagel5)
-    expect(basket.items).not.toContain(bagel5)
-  })
-  //bagel 6 add and remove//
-  it('Add item to basket', () => {
-    basket.addItem(bagel6)
-    expect(basket.items).toContain(bagel6)
-  })
-  it('Remove item from basket', () => {
-    basket.removeItem(bagel6)
-    expect(basket.items).not.toContain(bagel6)
-  })
-  //bagel 7 add and remove//
-  it('Add item to basket', () => {
-    basket.addItem(bagel7)
-    expect(basket.items).toContain(bagel7)
-  })
-  it('Remove item from basket', () => {
-    basket.removeItem(bagel7)
-    expect(basket.items).not.toContain(bagel7)
+
+  // Basket too full section
+  it('stops the basket from overfilling', () => {
+    
   })
 })
+
+module.exports = BagelBasket
