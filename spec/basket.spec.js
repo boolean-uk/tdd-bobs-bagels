f88575f33052faa6c0ffa1a48324bf2119caaa71
Chloe Zermatten <chloezermatten@gmail.com>
2023-12-13 12:09:30 +0000

    add tests for .addToBasket()

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -1,1 +1,32 @@
-// const {basketItem, basket} = require('../src/basket.js') 
+const { BasketItem, Basket } = require('../src/basket.js')
+
+describe('add to basket', () => {
+  it('the item, which was not already in the basket, is added', () => {
+    const basketItem1 = new BasketItem('BGLO', '0.49', 'Bagel', 'Onion')
+    const basketItem2 = new BasketItem('BGLP', '0.39', 'Bagel', 'Plain')
+    const basket1 = new Basket([basketItem1, basketItem2])
+
+    const basketItemToAdd = new BasketItem(
+      'BGLE',
+      '0.49',
+      'Bagel',
+      'Everything'
+    )
+
+    const result = basket1.addToBasket(basketItemToAdd)
+    const expected = new Basket([basketItem1, basketItem2, basketItemToAdd])
+
+    expect(result).toEqual(expected)
+  })
+
+  it('the item is already in the basket', () => {
+    const basketItem1 = new BasketItem('BGLO', '0.49', 'Bagel', 'Onion')
+    const basketItem2 = new BasketItem('BGLP', '0.39', 'Bagel', 'Plain')
+    const basket1 = new Basket([basketItem1, basketItem2])
+
+    const result = basket1.addToBasket(basketItem1)
+    const expected = 'this item is already in your basket!'
+
+    expect(result).toEqual(expected)
+  })
+})
