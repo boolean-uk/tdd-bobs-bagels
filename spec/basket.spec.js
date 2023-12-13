2866940feed807b62e14c45e55cc0f3058cc00e2
Lukas Dembicki <lad_777@hotmail.com>
2023-12-13 12:46:55 +0000

    invalid sku test added and failed. RED

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -2,10 +2,17 @@ const Basket = require('../src/basket.js')
 
 describe('Bobs bagels basket methods', () => {
   describe('/ adding basket items', () => {
+    const basket = new Basket()
+    beforeEach(() => {
+      basket.basketList = []
+    })
     it(' /adding a valid sku', () => {
-      const basket = new Basket()
       const newItem = basket.addItem('BGLO')
       expect(newItem).toEqual('Bagel')
+    })
+    it('/if sku is invalid', () => {
+      const newItem = basket.addItem('XXXX')
+      expect(newItem).toEqual('item not found')
     })
   })
 })
