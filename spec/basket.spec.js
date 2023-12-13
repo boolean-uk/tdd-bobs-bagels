6e628638e37da23f0bc0b8a7520efc78863b5880
Lukas Dembicki <lad_777@hotmail.com>
2023-12-13 12:38:43 +0000

    addItem method complete, first test pass. GREEN

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -1,9 +1,10 @@
 const Basket = require('../src/basket.js')
 
 describe('Bobs bagels basket methods', () => {
-  describe('adding basket items', () => {
-    it('adding a valid sku', () => {
-      const newItem = Basket.addItem('BGLO')
+  describe('/ adding basket items', () => {
+    it(' /adding a valid sku', () => {
+      const basket = new Basket()
+      const newItem = basket.addItem('BGLO')
       expect(newItem).toEqual('Bagel')
     })
   })
--- src/basket.js
+++ src/basket.js
@@ -1,7 +1,18 @@
+const inventory = require('../inventory')
+
 class Basket {
-  constructor() {}
+  constructor() {
+    this.basketList = []
+  }
+
+  addItem(sku) {
+    const itemToAdd = inventory.find((item) => item.sku === sku)
+    this.basketList.push(itemToAdd)
+    return itemToAdd.name
+  }
 }
 
-module.exports = {
-  Basket
-}
+const basket = new Basket()
+console.log(basket.addItem('BGLO'))
+
+module.exports = Basket
