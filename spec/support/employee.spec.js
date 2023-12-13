e2668972a626432770f253bd372cd16ed1d8a86c
Rebecca Noy <rebecca.noy@gmx.de>
2023-12-13 17:37:52 +0100

    add test to creation manager

--- spec/support/employee.spec.js
+++ spec/support/employee.spec.js
@@ -2,7 +2,12 @@ import Employee from "./employee.js"
 
 describe("Employee tests", () => {
   it("creation of worker", () => {
-    const test = new Employee("Tina", "worker")
-    expect(test.role).toEqual("worker")
+    const Tina = new Employee("Tina", "worker")
+    expect(Tina.role).toEqual("worker")
+  })
+
+  it("creation of manager", () => {
+    const Bob = new Employee("Bob", "manager")
+    expect(Bob.role).toEqual("manager")
   })
 })
