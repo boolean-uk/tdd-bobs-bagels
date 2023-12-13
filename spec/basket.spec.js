e5a6d033b63fdf614e04c2137c1173623c053c7f
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-12-13 16:22:35 +0000

    reformat: wrap describes in Basket describe

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -1,70 +1,72 @@
 import Basket from '../src/basket.js'
 
-describe('add bagel to basket', () => {
-  const basket = new Basket()
+describe('Basket', () => {
+  describe('add bagel to basket', () => {
+    const basket = new Basket()
 
-  beforeEach(() => {
-    basket._list = []
+    beforeEach(() => {
+      basket._list = []
+    })
+
+    it('add items to basket', () => {
+      const result1 = basket.add('BGLE')
+      const result2 = basket.add('BGSE')
+
+      expect(basket.list[0].sku).toEqual('BGLE')
+      expect(basket.list[0].price).toEqual('0.49')
+      expect(basket.list[0].name).toEqual('Bagel')
+      expect(basket.list[0].variant).toEqual('Everything')
+
+      expect(basket.list[1].sku).toEqual('BGSE')
+      expect(basket.list[1].price).toEqual('2.99')
+      expect(basket.list[1].name).toEqual('Bagel Sandwich')
+      expect(basket.list[1].variant).toEqual('Everything')
+      expect(basket.list[1].fillings).toEqual(['Bacon', 'Egg', 'Cheese'])
+
+      expect(result1).toEqual('item added')
+      expect(result2).toEqual('item added')
+    })
   })
 
-  it('add items to basket', () => {
-    const result1 = basket.add('BGLE')
-    const result2 = basket.add('BGSE')
+  describe('find item in inventory', () => {
+    const basket = new Basket()
 
-    expect(basket.list[0].sku).toEqual('BGLE')
-    expect(basket.list[0].price).toEqual('0.49')
-    expect(basket.list[0].name).toEqual('Bagel')
-    expect(basket.list[0].variant).toEqual('Everything')
+    beforeEach(() => {
+      basket._inventory = [
+        {
+          sku: 'AAAA',
+          price: '5',
+          name: 'Donut',
+          variant: 'Sesame'
+        },
+        {
+          sku: 'BBBB',
+          price: '10',
+          name: 'Lettuce',
+          variant: ''
+        },
+        {
+          sku: 'CCCC',
+          price: '15',
+          name: 'Tomato',
+          variant: ''
+        }
+      ]
+    })
 
-    expect(basket.list[1].sku).toEqual('BGSE')
-    expect(basket.list[1].price).toEqual('2.99')
-    expect(basket.list[1].name).toEqual('Bagel Sandwich')
-    expect(basket.list[1].variant).toEqual('Everything')
-    expect(basket.list[1].fillings).toEqual(['Bacon', 'Egg', 'Cheese'])
+    it('find items in inventory', () => {
+      const result = basket.findInventoryItem('AAAA')
 
-    expect(result1).toEqual('item added')
-    expect(result2).toEqual('item added')
-  })
-})
+      expect(result.sku).toEqual('AAAA')
+      expect(result.price).toEqual('5')
+      expect(result.name).toEqual('Donut')
+      expect(result.variant).toEqual('Sesame')
+    })
 
-describe('find item in inventory', () => {
-  const basket = new Basket()
+    it('sku not found in inventory', () => {
+      const result = basket.findInventoryItem('DDDD')
 
-  beforeEach(() => {
-    basket._inventory = [
-      {
-        sku: 'AAAA',
-        price: '5',
-        name: 'Donut',
-        variant: 'Sesame'
-      },
-      {
-        sku: 'BBBB',
-        price: '10',
-        name: 'Lettuce',
-        variant: ''
-      },
-      {
-        sku: 'CCCC',
-        price: '15',
-        name: 'Tomato',
-        variant: ''
-      }
-    ]
-  })
-
-  it('find items in inventory', () => {
-    const result = basket.findInventoryItem('AAAA')
-
-    expect(result.sku).toEqual('AAAA')
-    expect(result.price).toEqual('5')
-    expect(result.name).toEqual('Donut')
-    expect(result.variant).toEqual('Sesame')
-  })
-
-  it('sku not found in inventory', () => {
-    const result = basket.findInventoryItem('DDDD')
-
-    expect(result).toEqual('item not found')
+      expect(result).toEqual('item not found')
+    })
   })
 })
