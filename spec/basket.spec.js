d55cc8b79048746f28e43e5381c91d3f60545387
Satoki <satokiito@gmail.com>
2023-12-13 13:31:26 +0000

    part 1 remove bagel tests added

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -19,13 +19,13 @@ describe('Basket', () => {
       expect(result).toEqual(exampleBagel)
     })
 
-    it('bagel added is not a valid bagel type in bagel inventory', () => {
+    it('bagel being added does not exist in bagel inventory', () => {
       const result = b.addBagel()
       expect(result).toEqual('bagel does not exist, check bagel sku')
     })
 
     it('adds selected bagel to user basket', () => {
-      const exampleBagel = [
+      const exampleBasket = [
         {
           sku: 'BGLO',
           price: '0.49',
@@ -35,7 +35,48 @@ describe('Basket', () => {
       ]
       b.addBagel('BGLO')
       const result = b.getBasket()
+      expect(result).toEqual(exampleBasket)
+    })
+  })
+
+  describe('Remove Bagel From Basket', () => {
+    it('removed bagel must be returned as an object', () => {
+      const exampleBagel = {
+        sku: 'BGLO',
+        price: '0.49',
+        name: 'Bagel',
+        variant: 'Onion'
+      }
+      const result = b.removeBagel('BGLO')
       expect(result).toEqual(exampleBagel)
+    })
+
+    it('bagel being removed does not exist in basket', () => {
+      const result = b.removeBagel()
+      expect(result).toEqual('bagel does not exist, check bagel sku')
+    })
+
+    it('removes selected bagel from basket', () => {
+      const exampleBasket = [
+        {
+          sku: 'BGLP',
+          price: '0.39',
+          name: 'Bagel',
+          variant: 'Plain'
+        },
+        {
+          sku: 'BGLE',
+          price: '0.49',
+          name: 'Bagel',
+          variant: 'Everything'
+        }
+      ]
+      b.addBagel('BGLO')
+      b.addBagel('BGLP')
+      b.addBagel('BGLE')
+      b.removeBagel('BGLO')
+      const result = b.getBasket()
+      expect(result).toEqual(exampleBasket)
     })
   })
 })
