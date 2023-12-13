bd31435b2f55b87cf71496fbfaa69d1e8a1d6d81
callumhayden <callum.hayden@gmail.com>
2023-12-13 12:21:16 +0000

    remove feature and testing is working. Just need need to clean code up since ive got more code than needed

--- spec/Basket.spec.js
+++ spec/Basket.spec.js
@@ -47,33 +47,67 @@ describe('BagelBasket', () => {
   beforeEach(() => {
     basket = new BagelBasket()
   })
-
+//bagel 1 add and remove//
   it('Add item to basket', () => {
     basket.addItem(bagel1)
     expect(basket.items).toContain(bagel1)
   })
+  it('Remove item from basket', () => {
+    basket.removeItem(bagel1)
+    expect(basket.items).not.toContain(bagel1)
+  })
+  //bagel 2 add and remove//
   it('Add item to basket', () => {
     basket.addItem(bagel2)
     expect(basket.items).toContain(bagel2)
   })
+  it('Remove item from basket', () => {
+    basket.removeItem(bagel2)
+    expect(basket.items).not.toContain(bagel2)
+  })
+  //bagel 3 add and remove//
   it('Add item to basket', () => {
     basket.addItem(bagel3)
     expect(basket.items).toContain(bagel3)
   })
+  it('Remove item from basket', () => {
+    basket.removeItem(bagel3)
+    expect(basket.items).not.toContain(bagel3)
+  })
+  //bagel 4 add and remove//
   it('Add item to basket', () => {
     basket.addItem(bagel4)
     expect(basket.items).toContain(bagel4)
   })
+  it('Remove item from basket', () => {
+    basket.removeItem(bagel4)
+    expect(basket.items).not.toContain(bagel4)
+  })
+  //bagel 5 add and remove//
   it('Add item to basket', () => {
     basket.addItem(bagel5)
     expect(basket.items).toContain(bagel5)
   })
+  it('Remove item from basket', () => {
+    basket.removeItem(bagel5)
+    expect(basket.items).not.toContain(bagel5)
+  })
+  //bagel 6 add and remove//
   it('Add item to basket', () => {
     basket.addItem(bagel6)
     expect(basket.items).toContain(bagel6)
   })
+  it('Remove item from basket', () => {
+    basket.removeItem(bagel6)
+    expect(basket.items).not.toContain(bagel6)
+  })
+  //bagel 7 add and remove//
   it('Add item to basket', () => {
     basket.addItem(bagel7)
     expect(basket.items).toContain(bagel7)
+  })
+  it('Remove item from basket', () => {
+    basket.removeItem(bagel7)
+    expect(basket.items).not.toContain(bagel7)
   })
 })
--- src/basket.js
+++ src/basket.js
@@ -9,6 +9,16 @@ class BagelBasket {
       this.items.push(item)
     }
   }
+
+  removeItem(itemToRemove) {
+    const index = this.items.findIndex(
+      (item) => item.name === itemToRemove.name
+    )
+    if (index === -1) {
+      return 'Item not found'
+    }
+    this.items.splice(index, 1)
+  }
 }
 
 module.exports = BagelBasket
