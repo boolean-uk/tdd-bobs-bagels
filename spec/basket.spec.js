46ef8c6ff2ddbd5b8a60643f0e696b810bde675e
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-12-13 12:34:11 +0000

    add: basket.inventory spec

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -9,3 +9,44 @@ describe('add bagel to basket', () => {
     expect(basket.list[0].sku).toEqual('BGLO')
   })
 })
+
+describe('find item in inventory', () => {
+  const basket = new Basket()
+
+  beforeEach(() => {
+    basket._inventory = [
+      {
+        sku: 'AAAA',
+        price: '5',
+        name: 'Donut',
+        variant: 'Sesame'
+      },
+      {
+        sku: 'BBBB',
+        price: '10',
+        name: 'Lettuce',
+        variant: ''
+      },
+      {
+        sku: 'CCCC',
+        price: '15',
+        name: 'Tomato',
+        variant: ''
+      }
+    ]
+  })
+
+  it('find items in inventory', () => {
+    const result = basket.inventory
+
+    expect(result[0].sku).toEqual('AAAA')
+    expect(result[1].sku).toEqual('BBBB')
+    expect(result[2].sku).toEqual('CCCC')
+    expect(result[0].price).toEqual('5')
+    expect(result[1].price).toEqual('10')
+    expect(result[2].price).toEqual('15')
+    expect(result[0].name).toEqual('Donut')
+    expect(result[1].name).toEqual('Lettuce')
+    expect(result[2].name).toEqual('Tomato')
+  })
+})
