73c4ef749677ced5725089a25153ec51b607f70f
Chloe Zermatten <chloezermatten@gmail.com>
2023-12-13 12:28:47 +0000

    add .addToBasket() method

--- src/basket.js
+++ src/basket.js
@@ -2,8 +2,33 @@ class Basket {
   constructor() {
     this.list = []
   }
+
+  addToBasket(item) {
+    if (this.list.find((i) => i.sku === item.sku)) {
+      return 'this item is already in your basket!'
+    }
+    this.list.push(item)
+    return this.list
+  }
 }
 
+class BasketItem {
+  constructor(sku, price, name, variant) {
+    this.sku = sku
+    this.price = price
+    this.name = name
+    this.variant = variant
+  }
+}
+
+const basketItem1 = new BasketItem('BGLO', '0.49', 'Bagel', 'Onion')
+const basket = new Basket()
+
+console.log(basket)
+basket.addToBasket(basketItem1)
+console.log(basket)
+
 module.exports = {
-  Basket
+  Basket,
+  BasketItem
 }
