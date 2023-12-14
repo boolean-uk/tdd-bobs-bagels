62e7756fd2997c0d6da3a868a52e5c7d2f8fbcc6
Rebecca Noy <rebecca.noy@gmx.de>
2023-12-14 23:29:37 +0100

    test call rule calculation for specials

--- src/defaultSpecials.js
+++ src/defaultSpecials.js
@@ -1,29 +1,29 @@
 import Special from "./specials.js"
 
+const calcSpecial = (amount, price, piecesInSpecial, priceForSpecial) => {
+  const numOfSpecials = parseInt(Math.floor( amount / piecesInSpecial ))
+  const numOfNormalRate = amount - numOfSpecials * piecesInSpecial
+  return {
+    originalTotal: (amount * price).toFixed(2),
+    actualTotal: (numOfSpecials * priceForSpecial + numOfNormalRate * price).toFixed(2)
+  }
+}
+
 const defaultSpecials = [
   new Special("BGLO", (amount, price) => {
-    const numOfSpecials = parseInt(Math.floor( amount / 6 ))
-    const numOfNormalRate = amount - numOfSpecials
-    return {
-      originalTotal: amount * price,
-      actualTotal: numOfSpecials * 2.49 + numOfNormalRate * price
-    }
+    const piecesInSpecial = 6
+    const priceForSpecial = 2.49
+    return calcSpecial(amount, price, piecesInSpecial, priceForSpecial)
   }),
   new Special("BGLP", (amount, price) => {
-    const numOfSpecials = parseInt(Math.floor( amount / 12 ))
-    const numOfNormalRate = amount - numOfSpecials
-    return {
-      originalTotal: amount * price,
-      actualTotal: numOfSpecials * 3.99 + numOfNormalRate * price
-    }
+    const piecesInSpecial = 12
+    const priceForSpecial = 3.99
+    return calcSpecial(amount, price, piecesInSpecial, priceForSpecial)
   }),
   new Special("BGLE", (amount, price) => {
-    const numOfSpecials = parseInt(Math.floor( amount / 6 ))
-    const numOfNormalRate = amount - numOfSpecials
-    return {
-      originalTotal: amount * price,
-      actualTotal: numOfSpecials * 3.49 + numOfNormalRate * price
-    }
+    const piecesInSpecial = 6
+    const priceForSpecial = 2.49
+    return calcSpecial(amount, price, piecesInSpecial, priceForSpecial)
   })
 ]
 
