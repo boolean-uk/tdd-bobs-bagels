a44caa657430365858a773c0f67ab5bc3d37ecd5
Nazar Tymiv <nazartumiv@gmail.com>
2023-12-14 21:40:48 +0000

    add special offers solution :)

--- src/basket.js
+++ src/basket.js
@@ -1,5 +1,11 @@
 const inventory = require('../inventory')
 
+const specialOffers = [
+  { quantity: 6, sku: 'BGLO', price: 2.49 },
+  { quantity: 12, sku: 'BGLP', price: 3.99 },
+  { quantity: 6, sku: 'BGLE', price: 2.49 }
+]
+
 class Basket {
   constructor(capacity) {
     this.capacity = capacity
@@ -64,16 +70,82 @@ class Basket {
   }
 
   totalSum() {
-    const sum = this.basketList
-      .map((item) => item.price * item.quantity)
+    let totalSum = 0
+
+    let localBasketList = this.basketList
+
+    const specialBasket = localBasketList.filter(
+      (item) => item.sku === 'COF' || item.sku === 'BGLP'
+    )
+
+    if (specialBasket.length === 2) {
+      const specialQuantity1 = specialBasket[0].quantity
+      const specialQuantity2 = specialBasket[1].quantity
+
+      const minusQuality = (num) => {
+        specialBasket[0].quantity -= num
+        specialBasket[1].quantity -= num
+      }
+
+      if (
+        specialQuantity1 < specialQuantity2 ||
+        specialQuantity1 === specialQuantity2
+      ) {
+        totalSum += 1.25 * specialQuantity1
+        minusQuality(specialQuantity1)
+      } else if (specialQuantity2 < specialQuantity1) {
+        totalSum += 1.25 * specialQuantity2
+        minusQuality(specialQuantity2)
+      }
+
+      specialBasket.forEach((item) => {
+        if (item.quantity === 0) {
+          localBasketList = localBasketList.filter((i) => i.sku !== item.sku)
+        } else {
+          localBasketList.find((i) => item.sku === i.sku).quantity =
+            item.quantity
+        }
+      })
+    }
+
+    totalSum += localBasketList
+      .map((item) => {
+        let productTotal = 0
+        let localQuantity = item.quantity
+        const specialProduct =
+          specialOffers.find((i) => i.sku === item.sku) || null
+
+        if (specialProduct && item.quantity >= specialProduct.quantity) {
+          const countSpecial = Math.floor(
+            item.quantity / specialProduct.quantity
+          )
+
+          productTotal += specialProduct.price * countSpecial
+          localQuantity -= specialProduct.quantity * countSpecial
+        }
+
+        productTotal += item.price * localQuantity
+        // console.log(item)
+
+        return Number(productTotal)
+      })
       .reduce((a, b) => a + b, 0)
 
-    if (sum > 0) {
-      return sum
+    if (totalSum > 0) {
+      return Number(totalSum.toFixed(2))
     }
 
     return 'Your basket is empty'
   }
 }
+
+// const basket = new Basket(25)
+
+// basket.addBagel('BGLE')
+// basket.addBagel('BGLP')
+// basket.addBagel('BGLP')
+// basket.addBagel('COF')
+
+// console.log(basket.totalSum())
 
 module.exports = Basket
