90cfa2a787bfabd0dc9c6cbf47a70244d110a11f
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2023-12-17 14:07:37 +0100

    test adding and removing an item

---
+++ spec/support/basket.spec.js
@@ -0,0 +1,32 @@
+const { ShoppingBasket } = require('../../src/basket')
+
+describe('ShoppingBasket', () => {
+  let basket
+  const items = [
+    { sku: 'BGLO', price: '0.49', name: 'Bagel', variant: 'Onion' },
+    { sku: 'BGLP', price: '0.39', name: 'Bagel', variant: 'Plain' },
+    { sku: 'BGLE', price: '0.49', name: 'Bagel', variant: 'Everything' },
+    { sku: 'BGLS', price: '0.49', name: 'Bagel', variant: 'Sesame' },
+    { sku: 'COF', price: '0.39', name: 'Coffee', variant: '' },
+    {
+      sku: 'BGSE',
+      price: '2.99',
+      name: 'Bagel Sandwich',
+      variant: 'Everything'
+    },
+    { sku: 'BGSS', price: '4.99', name: 'Bagel', variant: 'Sesame' }
+  ]
+  beforeEach(() => {
+    basket = new ShoppingBasket()
+  })
+  items.forEach((item, index) => {
+    describe(`Item ${index + 1}`, () => {
+      it('adds and removes an item from the basket', () => {
+        basket.addItem(item)
+        expect(basket.containsItem(item)).toBeTruthy()
+        basket.removeItem(item)
+        expect(basket.containsItem(item)).toBeFalsy()
+      })
+    })
+  })
+})
