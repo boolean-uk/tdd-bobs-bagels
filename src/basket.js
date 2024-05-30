f28e4145ea29ed3625c3a6601626de73a82874ff
Angus Townsley <angustownsley@gmail.com>
2024-05-30 13:36:34 +0100

    Add removeItem method to basket class

--- src/basket.js
+++ src/basket.js
@@ -1,33 +1,43 @@
-class Basket{
-    constructor() {
-        this.contents = []
+class Basket {
+  constructor() {
+    this.contents = []
+  }
+
+  addItem(item) {
+    const itemInBasket = this.contents.find((element) => {
+      return element.name === item.name
+    })
+
+    if (itemInBasket) {
+      itemInBasket.quantity++
+      return this.contents
     }
 
-    addItem(item) {
-        const itemInBasket = this.contents.find((element) => {return element.name === item.name})
+    const newItem = { ...item, quantity: 1 }
 
-        if(itemInBasket){
-            itemInBasket.quantity++
-            return;
-        }
-        
-        const newItem = {...item, quantity:1}
+    this.contents.push(newItem)
 
-        this.contents.push(newItem)
-    }
+    return this.contents
+  }
+
+  removeItem(item) {
+    this.contents = this.contents.filter((element) => {return !element.name === item.name})
+    
+    return this.contents
+  }
 }
 
-class Bagel{
-    constructor(name, price) {
-        if(typeof name !== "string" || typeof price !== "number"){
-            throw new Error("Invalid argument types")
-        }
-        
-        this.name = name
-        this.price = price
+class Bagel {
+  constructor(name, price) {
+    if (typeof name !== 'string' || typeof price !== 'number') {
+      throw new Error('Invalid argument types')
     }
+
+    this.name = name
+    this.price = price
+  }
 }
 
-export {Bagel}
+export { Bagel }
 
 export default Basket
