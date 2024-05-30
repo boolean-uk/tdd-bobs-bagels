327568fbdfa7ef222bdd68feb8f1f1a6984c887e
Will Baxter <bax.will@googlemail.com>
2024-05-30 11:03:00 +0100

    Added test to check if addItem function works

---
+++ spec/basket.spec.js
@@ -0,0 +1,2 @@
+import Basket from '../src/basket.js'
+import 'jasmine-expect'
---
+++ src/basket.js
@@ -0,0 +1,10 @@
+import data from '../inventory.json' assert { type: 'json' }
+
+const { inventory } = data
+
+class Basket {
+
+}
+
+
+export default Basket
