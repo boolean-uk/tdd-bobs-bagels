8487f4f545912e3b8d5b0d985e690d0cbea0f80b
Will Baxter <bax.will@googlemail.com>
2024-05-30 12:17:56 +0100

    Added check out for total price functionality

--- src/basket.js
+++ src/basket.js
@@ -12,12 +12,12 @@ class Basket {
     if (this.contents.length >= this.contentLimit) {
       return 'Sorry, your basket is full'
     }
-    const itemToAdd = inventory.find((item) => item.sku === SKU)
+    const itemToAdd = inventory.find(item => item.sku === SKU)
     this.contents.push(itemToAdd)
   }
 
   removeItem(SKU) {
-    const itemToRemove = this.contents.find((item) => item.sku === SKU)
+    const itemToRemove = this.contents.find(item => item.sku === SKU)
     if (itemToRemove) {
       const index = this.contents.indexOf(itemToRemove)
       this.contents.splice(index, 1)
@@ -26,8 +26,14 @@ class Basket {
   }
 
   checkPrice(SKU) {
-    const item = inventory.find((item) => item.sku === SKU)
+    const item = inventory.find(item => item.sku === SKU)
     return item.price
+  }
+
+  checkOut() {
+    let totalPrice = 0
+    this.contents.forEach(item => {totalPrice += Number(item.price)})
+    return totalPrice.toString()
   }
 }
 
