7f9a4f1b01956971cdf7756e7ab2f96678c73fdc
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-05-30 12:41:04 +0200

    create test to see if bagellist has items

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -1,1 +1,14 @@
+import BagelList from '../src/basket.js'
 
+describe('Bagel list', () => {
+  let bagelList
+
+  beforeEach(() => {
+    bagelList = new BagelList()
+  })
+
+  it('bagel list exists and has items in it', () => {
+    expect(bagelList).toBeInstanceOf(BagelList)
+    expect(bagelList.length).toBe(7)
+  })
+})
---
+++ src/basket.js
@@ -0,0 +1,7 @@
+import json from "../inventory.json" assert { type: "json" }
+
+class BagelList {
+
+}
+
+export default BagelList
