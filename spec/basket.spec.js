a3d683115ee6d9c2b9cb927174058437cefa1aff
Leonardo Lodi <your_email@address>
2024-05-30 12:00:29 +0100

    test add function

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -1,4 +1,4 @@
-import Basket from '../src/basket.js'
+import Basket, { Order } from '../src/basket.js'
 
 describe('Basket', () => {
   let basket
@@ -9,5 +9,15 @@ describe('Basket', () => {
 
   it('should exist', () => {
     expect(basket).toBeInstanceOf(Basket)
+  })
+
+  it('should be added an order', () => {
+    basket.add('bagel')
+    basket.add('banana')
+
+    expect(basket.orders).toEqual([
+      new Order(1, 'bagel'),
+      new Order(2, 'banana')
+    ])
   })
 })
