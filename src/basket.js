0f2904f4e5f2ae967fd89c53e9b6a4d12b3e2912
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-05-30 12:56:19 +0200

    create basket class

--- src/basket.js
+++ src/basket.js
@@ -3,7 +3,10 @@ import json from "../inventory.json" assert { type: "json" }
 const bagels = json.inventory
 
 class Basket {
-
+    constructor(capacity = 4) {
+        this.basket = []
+        this.capacity = capacity
+    }
 }
 
 export default Basket
