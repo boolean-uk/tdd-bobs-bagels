3d4552ce9eaf05400d4608129d70b95128821d12
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-05-30 13:05:57 +0200

    add function to add bagel to basket

--- src/basket.js
+++ src/basket.js
@@ -7,6 +7,16 @@ class Basket {
         this.basket = []
         this.capacity = capacity
     }
+
+    add(sku) {
+        const found = bagels.find((bagel) => bagel.sku === sku)
+
+        if(this.basket.length < this.capacity && found) {
+            this.basket.push(found)
+
+            return this.basket
+        }
+    }
 }
 
 export default Basket
