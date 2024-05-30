5b1cac58885da9b9ae9c040114021b0d378ae8ad
Will Baxter <bax.will@googlemail.com>
2024-05-30 11:26:50 +0100

    Added functionality to prevent items exceeding basket content limit

--- src/basket.js
+++ src/basket.js
@@ -3,21 +3,23 @@ import data from '../inventory.json' assert { type: 'json' }
 const { inventory } = data
 
 class Basket {
-    constructor(contentLimit = 5) {
-        this.contents = []
-        this.contentLimit = contentLimit
-    }
+  constructor(contentLimit = 5) {
+    this.contents = []
+    this.contentLimit = contentLimit
+  }
 
-    addItem(SKU) {
-        const itemToAdd = inventory.find((item) => item.sku === SKU)
-        this.contents.push(itemToAdd)
+  addItem(SKU) {
+    if (this.contents.length >= this.contentLimit) {
+      return 'Sorry, your basket is full'
     }
+    const itemToAdd = inventory.find((item) => item.sku === SKU)
+    this.contents.push(itemToAdd)
+  }
 
-    removeItem(SKU) {
-        const indexToRemove = this.contents.indexOf(SKU)
-        this.contents.splice(indexToRemove, 1)
-    }
+  removeItem(SKU) {
+    const indexToRemove = this.contents.indexOf(SKU)
+    this.contents.splice(indexToRemove, 1)
+  }
 }
-
 
 export default Basket
