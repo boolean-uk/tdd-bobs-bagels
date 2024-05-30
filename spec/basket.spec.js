5a3307a613bb927cdad6b6451e20790fb857d3fa
Will Baxter <bax.will@googlemail.com>
2024-05-30 11:09:24 +0100

    Add item function working correctly

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -1,2 +1,11 @@
 import Basket from '../src/basket.js'
 import 'jasmine-expect'
+
+describe('Bagel basket', () => {
+  it('should be able to have new items added', () => {
+    const basket = new Basket()
+    basket.addItem('BGLO')
+
+    expect(basket.contents.length).toEqual(1)
+  })
+})
--- src/basket.js
+++ src/basket.js
@@ -3,7 +3,15 @@ import data from '../inventory.json' assert { type: 'json' }
 const { inventory } = data
 
 class Basket {
+    constructor() {
+        this.contents = []
+        this.contentLimit = 5
+    }
 
+    addItem(SKU) {
+        const itemToAdd = inventory.find((item) => item.sku === SKU)
+        this.contents.push(itemToAdd)
+    }
 }
 
 
