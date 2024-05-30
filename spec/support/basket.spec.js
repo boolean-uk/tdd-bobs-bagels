2d399888f86c3e60479caffeb9ff690e904d80b9
Tim Zoltie <t.zoltie@gmail.com>
2024-05-30 11:27:16 +0100

    item class created

--- spec/support/basket.spec.js
+++ spec/support/basket.spec.js
@@ -1,11 +1,17 @@
-import Basket from "../../src/basket.js";
+import Basket, { Item } from "../../src/basket.js";
 
 describe('Basket', () => {
     let basket
     beforeEach(() => {
         basket = new Basket()
     })
-    it('shold exist', () => {
+    it('should exist', () => {
         expect(basket).toBeInstanceOf(Basket)
+    })
+    it('should add an item to the basket', () => {
+        const item = basket.addToBasket('poppyseed bagel')
+        expect(item).toBeInstanceOf(Item)
+        expect(item.id).toBe(1)
+        expect(item.title).toBe('poppyseed bagel')
     })
 })
--- src/basket.js
+++ src/basket.js
@@ -1,5 +1,15 @@
 class Basket {
 
+
 }
+
+class Item {
+    constructor(id, title) {
+        this.id = id
+        this.title = title
+    }
+}
+
+export { Item }
 
 export default Basket
