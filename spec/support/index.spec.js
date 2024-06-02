9d4c38e98bf2a77667a9dce0f7a0c34449d40ad6
Alistair Henshaw <alistairhenshaw@gmail.com>
2024-06-02 21:01:25 +0100

    made tests for adding items to the basket

--- spec/support/index.spec.js
+++ spec/support/index.spec.js
@@ -1,9 +1,36 @@
+import Basket from "../../src/basket.js"
+
 describe("Basket", () => {
+    let basket
     beforeEach(() => {
-        const basket = new Basket()
+        basket = new Basket()
     })
 
     it("should exist", () => {
         expect(basket).toBeInstanceOf(Basket)
     })
+
+    it("add an item to the basket", () => {
+        basket.addItem("seed bagel")
+        expect(basket.basket).toBe([{name: "seed bagel", price: 1.99, quantity: 1}])
+    })
+
+    it("shouldn't add an item which doesn't exist", () => {
+        basket.addItem("spaghetti")
+        expect(basket.basket).toBe([])
+
+    })
+
+    it("be able to add multiple of the same item", () => {
+        basket.addItem("dirty bagel", 3)
+        expect(basket.basket).toBe([{name: "dirty bagel", price: 0.99, quantity: 3}])
+    })
+
+    it("refuse to add item/items if basket quantity will be exceeded", () => {
+        basket.addItem("dirty bagel", 3)
+        basket.addItem("seed bagel", 1)
+        basket.addItem("toasted bagel", 3)
+        expect(basket.basket).toBe([{name: "dirty bagel", price: 0.99, quantity: 3}, {name: "seed bagel", price: 1.99, quantity: 1}])
+    })
+
 })
