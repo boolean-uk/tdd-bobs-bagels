36d8be63b38910762357bbae53f4681b23b6ca0f
Kyle Vann <kyle.vann1996@icloud.com>
2024-06-04 12:11:11 +0100

    test 2

---
+++ spec/basket.test.js
@@ -0,0 +1,19 @@
+const Item = require('../src/item')
+const Basket = require('../src/basket')
+
+test('Basket class adds an item to the basket', () => {
+  const basket = new Basket()
+  const bagel = new Item('Plain Bagel', 1.5)
+  const response = basket.addItem(bagel)
+  expect(response).toBe('Added Plain Bagel to the basket.')
+  expect(basket.items).toContainEqual(bagel)
+})
+
+test('Basket class removes an item from the basket', () => {
+  const basket = new Basket()
+  const bagel = new Item('Plain Bagel', 1.5)
+  basket.addItem(bagel)
+  const response = basket.removeItem('Plain Bagel')
+  expect(response).toBe('Removed Plain Bagel from the basket.')
+  expect(basket.items).not.toContainEqual(bagel)
+})
--- src/basket.js
+++ src/basket.js
@@ -1,10 +1,31 @@
-const Item = require('./item');
+const Item = require('./item')
 
 class Basket {
   constructor(capacity = 5) {
-    this.items = [];
-    this.capacity = capacity;
+    this.items = []
+    this.capacity = capacity
+  }
+
+  addItem(item) {
+    if (this.isFull()) {
+      return 'Basket is full. Cannot add more items.'
+    }
+    this.items.push(item)
+    return `Added ${item.name} to the basket.`
+  }
+
+  removeItem(itemName) {
+    const index = this.items.findIndex((item) => item.name === itemName)
+    if (index !== -1) {
+      const removedItem = this.items.splice(index, 1)[0]
+      return `Removed ${removedItem.name} from the basket.`
+    }
+    return 'Item not found in the basket.'
+  }
+
+  isFull() {
+    return this.items.length >= this.capacity
   }
 }
 
-module.exports = Basket;
+module.exports = Basket
