574156b9cf31185cda18aa9ae1508551afaa8a3d
Farshad <f.bagdeli13@gmail.com>
2024-06-06 17:05:53 +0200

    remove bagel Done

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -19,11 +19,40 @@ describe('BasketManager', () => {
     expect(basketManager.basicCap).toBe(5)
   })
 
-  it('add bagel to basket', () => {
+  it('should add bagel to basket', () => {
     const result = basketManager.add('honey')
     expect(result).toBe('We dont have that bagel')
 
     basketManager.add('Chocolate')
     expect(basketManager.items.length).toBe(1)
+
+    basketManager.add('Chocolate')
+    basketManager.add('Chocolate')
+
+    expect(basketManager.items.length).toBe(3)
+  })
+
+  it('should remove a bagel from the basket', () => {
+    // const result = basketManager.remove('')
+    basketManager.add('Chocolate')
+    basketManager.add('Chocolate')
+    basketManager.add('Egg')
+    basketManager.add('Plain')
+    basketManager.add('Salt')
+
+    expect(() => basketManager.remove('Honey')).toThrowError(
+      'You dont have that bagel'
+    )
+
+    expect(basketManager.items[0].bName).toBe('Chocolate')
+    expect(basketManager.items[1].bName).toBe('Chocolate')
+    expect(basketManager.items[2].bName).toBe('Egg')
+    basketManager.remove('Plain')
+    basketManager.remove('Chocolate')
+    expect(basketManager.items).toEqual([
+      Object({ bName: 'Chocolate', bPrice: 2.99 }),
+      Object({ bName: 'Egg', bPrice: 3.99 }),
+      Object({ bName: 'Salt', bPrice: 0.99 })
+    ])
   })
 })
--- src/basket.js
+++ src/basket.js
@@ -20,6 +20,19 @@ class BasketManager {
       return this.items.push(found)
     }
   }
+
+  remove(bName) {
+    const found = this.items.find((item) => item.bName === bName)
+    if (!found) {
+      throw new Error('You dont have that bagel')
+    } else {
+      const index = this.items.findIndex((item) => {
+        return item.bName === bName
+      })
+      this.items.splice(index, 1)
+      return this.items
+    }
+  }
 }
 
 module.exports = BasketManager
